9f6a71b92e30020b63cee164a5c35e29e459e1e7
Liam Flood <liamjflood@gmail.com>
2017-04-23 22:57:43 +0200

    Create pythagoras.py

---
+++ math/pythagoras.py
@@ -0,0 +1,16 @@
+"""
+input two of the three side in right angled triangle and return the third. use "?" to indicate the unknown side. 
+"""
+
+def pythagoras(opposite,adjacent,hypotenuse):
+	if opposite == str("?"):
+		print ("Solving for opposite")
+		return ((h**2) - (a**2))**0.5
+	elif adjacent == str("?"):
+		print ("Solving for adjacent")
+		return ((h**2) - (o**2))**0.5
+	elif hypotenuse == str("?"):
+		print ("Solving for hypotenuse")
+		return ((o**2)+(a**2))**0.5
+	else:
+		return "you already know the answer!"
