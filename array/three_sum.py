f426cd64d9e95ac4a1463af0eee55a1ed8fa02f1
programmerysa <741437084@qq.com>
2017-04-25 14:55:16 +0800

    Update three_sum.py

--- array/three_sum.py
+++ array/three_sum.py
@@ -3,7 +3,7 @@ Given an array S of n integers, are there elements a, b, c in S
 such that a + b + c = 0?
 Find all unique triplets in the array which gives the sum of zero.
 
-Note: The solution set must not contain duplicate triplets.
+Note: The solution set must not    contain duplicate triplets.
 
 For example, given array S = [-1, 0, 1, 2, -1, -4],
 
