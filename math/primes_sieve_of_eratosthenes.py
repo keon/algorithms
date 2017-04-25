fab803ed5fa39d696c458ca620005d249134489b
Tejas Kumar Jaiswal <tejasjaiswal9711@gmail.com>
2017-04-25 11:13:51 +0530

    added a comment to let beginners know about the assert function in python

--- math/primes_sieve_of_eratosthenes.py
+++ math/primes_sieve_of_eratosthenes.py
@@ -4,6 +4,8 @@ Using sieve of Eratosthenes, primes(x) returns list of all primes less than x
 
 def primes(x):
     assert(x>=0)
+    # if you don't know the above assert function...
+    # telling the program to test that condition, and trigger an error if the condition is false.
     sieve = [1 for v in range(x+1)]     # Sieve
     primes = []                         # List of Primes
     for i in range(2,x+1):
