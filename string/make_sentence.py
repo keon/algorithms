9c295e556d8ee93b660ec2123e25bd0707257e01
yangroro <529yhs@gmail.com>
2017-04-26 17:51:29 +0900

    Rewrite make_sentence.py to python from C++

--- string/make_sentence.py
+++ string/make_sentence.py
@@ -2,44 +2,38 @@
 For a given string and dictionary, how many sentences can you make from the
 string, such that all the words are contained in the dictionary.
 
-// eg: for given string -> "appletablet"
-// "apple", "tablet"
-// "applet", "able", "t"
-// "apple", "table", "t"
-// "app", "let", "able", "t"
+eg: for given string -> "appletablet"
+"apple", "tablet"
+"applet", "able", "t"
+"apple", "table", "t"
+"app", "let", "able", "t"
 
-// "applet", {app, let, apple, t, applet} => 3
-// "thing", {"thing"} -> 1
+"applet", {app, let, apple, t, applet} => 3
+"thing", {"thing"} -> 1
 """
 
-// Example program
-#include <iostream>
-#include <string>
-#include <set>
-#include <vector>
+class MakeSentence(object):
 
-using namespace std;
+    def __init__(self, example_string, dictionarys):
+        self.count = 0
+        self.example_string = example_string
+        self.dictionarys = dictionarys
 
+    def run(self):
+        self.make_sentence(self.example_string, self.dictionarys)
+        print(self.count)
 
-bool foo(string s, set<string> dic, int &cnt){
-    if(s.empty())
-        return true;
+    def make_sentence(self, str_piece, dictionarys):
+        if len(str_piece) == 0:
+            return True
+        for i in range(0, len(str_piece)):
+            prefix, suffix = str_piece[0:i], str_piece[i:]
+            if ((prefix in dictionarys and suffix in dictionarys)
+                    or (prefix in dictionarys and self.make_sentence(suffix, dictionarys))):
+                self.count += 1
+        return True
 
-    for(int i=0;i<s.length();++i){
-        string prefix = s.substr(0,i), suffix = s.substr(i);
-       if((dic.count(prefix)&&dic.count(suffix))
-            ||(dic.count(prefix)&&foo(suffix,dic,cnt))){
-
-            cnt++;
-        }
-    }
-    return true;
-}
-int main()
-{
-    string s = "applet";
-    set<string> dic{"","app","let","t","apple","applet"};
-    int cnt = 0;
-    foo(s,dic,cnt);
-    cout<<cnt;
-}
+if __name__ == "__main__":
+    dictionarys = ["", "app", "let", "t", "apple", "applet"]
+    make_sentence = MakeSentence("applet", dictionarys)
+    make_sentence.run()
