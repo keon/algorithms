afe939b366e68005084f933af50c3571cb8c0b48
Quang Nguyen <quang2705@users.noreply.github.com>
2017-04-27 22:57:12 -0400

    Create counting_sort.py

---
+++ counting_sort.py
@@ -0,0 +1,24 @@
+  def couting_sort(arr):
+	"""Couting_sort
+		Sorting a array which has no element greater than k
+		Complexity: 0(n)
+	"""
+	k = max(arr)
+	temp_arr = [0]*(k+1)
+	for i in range(0,len(arr)):
+		temp_arr[arr[i]]= temp_arr[arr[i]]+1
+	#temp_array[i] is the times the number i appear in arr
+	for i in range(1, k+1):
+		temp_arr[i] = temp_arr[i] + temp_arr[i-1]
+	#temp_array[i] is the number of element less than or equal i in arr
+	result_arr = [0]*len(arr)
+	#creating a result_arr an put the element in a correct positon
+	for i in range(len(arr)-1,-1,-1):
+		result_arr[temp_arr[arr[i]]-1] = arr[i]
+		temp_arr[arr[i]] = temp_arr[arr[i]]-1
+
+	return result_arr 
+
+
+x =couting_sort([1,2,3,4,9,1,2,8,3,5,7,0,9,8,1,7,4,5])
+print (x)
