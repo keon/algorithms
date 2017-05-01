2b0c6110445293ef87913c4b70f61753d6d6ace7
Quang Nguyen <quang2705@users.noreply.github.com>
2017-05-01 10:52:35 -0400

    Update counting_sort.py

--- counting_sort.py
+++ counting_sort.py
@@ -1,11 +1,11 @@
 def counting_sort(arr):
-    """ Couting_sort
-    Sorting a array which has no element greater than k
-    Creating a new temp_arr,where temp_arr[i] contain the number of
-	element less than or equal to i in the arr
-	Then placing the number i into a correct position in the result_arr
-	return the result_arr
-	Complexity: 0(n)
+	"""Couting_sort
+		Sorting a array which has no element greater than k
+		Creating a new temp_arr,where temp_arr[i] contain the number of
+		element less than or equal to i in the arr
+		Then placing the number i into a correct position in the result_arr
+		return the result_arr
+		Complexity: 0(n)
 	"""
 	
 	m = min(arr)
@@ -15,11 +15,11 @@ def counting_sort(arr):
 		#save the change, so that we can convert the array back to all positive number
 		different = -m
 		for i in range (len(arr)):
-			arr[i] += -m
+			arr[i]+= -m
 	k = max(arr)
-	temp_arr = [0] * (k + 1)
+	temp_arr = [0]*(k+1)
 	for i in range(0,len(arr)):
-		temp_arr[arr[i]] = temp_arr[arr[i]] + 1
+		temp_arr[arr[i]]= temp_arr[arr[i]]+1
 	#temp_array[i] contain the times the number i appear in arr
 	
 	for i in range(1, k+1):
@@ -29,7 +29,7 @@ def counting_sort(arr):
 	result_arr = [0]*len(arr)
 	#creating a result_arr an put the element in a correct positon
 	for i in range(len(arr)-1,-1,-1):
-		result_arr[temp_arr[arr[i]]-1] = arr[i] - different
+		result_arr[temp_arr[arr[i]]-1] = arr[i]-different
 		temp_arr[arr[i]] = temp_arr[arr[i]]-1
 	
 	return result_arr 
@@ -41,5 +41,3 @@ if __name__ == "__main__":
 	y = counting_sort(negative_array)
 	print(x)
 	print(y)
-    
-	
