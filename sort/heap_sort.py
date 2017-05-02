7d9115aaa429f0a6453c8fcc75c77abc2bdaec93
Imanuel Chen <imanuelhchen@gmail.com>
2017-05-02 16:25:15 -0700

    Set up basic structure of code

---
+++ sort/heap_sort.py
@@ -0,0 +1,13 @@
+def heap_sort(arr):
+	""" Heapsort
+		Complexity: O(n log(n))
+	"""
+	pass
+
+def heapify(arr):
+	pass
+
+array = [1,5,65,23,57,1232,-1,-5,-2,242,100,4,423,2,564,9,0,10,43,64]
+print(array)
+heap_sort(array)
+print(array)
