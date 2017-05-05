53da2a3ad92724de2b87f21b8ba2d5c11e3f7ded
orenovadia <orenovad@gmail.com>
2017-05-05 17:34:19 +0300

    HashTable: reformat code (pep8)

--- map/hashtable.py
+++ map/hashtable.py
@@ -7,31 +7,33 @@ del Delete the key-value pair from the map using a statement of the form del map
 len() Return the number of key-value pairs stored in the map.
 in Return True for a statement of the form key in map, if the given key is in the map, False otherwise.
 """
+
+
 class HashTable(object):
-    def __init__(self, size = 11):
+    def __init__(self, size=11):
         self.size = size
-        self.keys = [None] * size # keys
-        self.values = [None] * size # values
+        self.keys = [None] * size  # keys
+        self.values = [None] * size  # values
 
     def put(self, key, value):
-        hashval = self.hash(key)
+        hash_value = self.hash(key)
 
-        if hashval not in keys:
-            self.keys[hashval] = key
-            self.values[hashval] = value
+        if hash_value not in keys:
+            self.keys[hash_value] = key
+            self.values[hash_value] = value
         else:
-            if self.keys[hashval] == key: # replace
-                self.values[hashval] = value
-            else: # probing
+            if self.keys[hash_value] == key:  # replace
+                self.values[hash_value] = value
+            else:  # probing
                 rehashval = self.rehash(key)
                 while self.keys[rehashval] is not None and \
-                        self.keys[rehashval] != key:
+                                self.keys[rehashval] != key:
                     rehashval = self.rehash(rehashval)
                 if keys[rehashval] is None:
                     self.keys[rehashval] = key
                     self.values[rehashval] = value
                 else:
-                    self.values[rehashval] = value # replacee
+                    self.values[rehashval] = value  # replacee
 
     def get(self, key):
         pass
@@ -39,16 +41,14 @@ class HashTable(object):
     def hash(self, key):
         return key % self.size
 
-    def rehash(self, oldhash):
+    def rehash(self, old_hash):
         """
         linear probing
         """
-        return (oldhash + 1) % self.size
+        return (old_hash + 1) % self.size
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return self.get(key)
 
     def __setitem__(self, key, value):
         self.put(key, value)
-
-
