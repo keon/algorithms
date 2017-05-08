60d1655a8595ecf9b312b1b1899b3f24484124f1
ofek <ofekmeister@gmail.com>
2017-05-07 22:51:34 -0400

    require known size

--- bit/find_missing_number.py
+++ bit/find_missing_number.py
@@ -1,13 +1,10 @@
 def find_missing_number(nums):
-    """Returns the missing number from a sorted sequence of integers
+    """Returns the missing number from a sorted list of integers
     in O(n) time and space.
 
-    >>> find_missing(i for i in range(-2000, 10000) if i != 1234)
+    >>> find_missing([i for i in range(-2000, 10000) if i != 1234])
     1234
     """
-
-    # Ensure indexable
-    nums = list(nums)
 
     # Force odd length
     if len(nums) & 1 == 0:
