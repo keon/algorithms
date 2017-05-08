0fb982506affb204a107f875083e30755f5f7c8d
ofek <ofekmeister@gmail.com>
2017-05-07 22:35:40 -0400

    fix whitespace

--- bit/find_missing_number.py
+++ bit/find_missing_number.py
@@ -1,7 +1,7 @@
 def find_missing_number(nums):
     """Returns the missing number from a sorted sequence of integers
     in O(n) time and space.
-    
+
     >>> find_missing(i for i in range(-2000, 10000) if i != 1234)
     1234
     """
@@ -18,4 +18,5 @@ def find_missing_number(nums):
         if num != 0:
             missing ^= num
         missing ^= i + 1
+
     return missing
