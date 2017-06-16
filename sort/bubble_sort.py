c18dfde1a572b53427c3f4f4e44c7891c3fc1e37
Semen Zhydenko <simeon.zhidenko@gmail.com>
2017-06-16 17:40:21 +0200

    Added bubble_sort.py

---
+++ sort/bubble_sort.py
@@ -0,0 +1,25 @@
+"""
+https://en.wikipedia.org/wiki/Bubble_sort
+
+Complexity: O(N^2)
+
+"""
+
+def bubble_sort(arr):
+
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
+
+    n = len(arr)
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(1, n - 1):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+
+array = [1, 5, 65, 23, 57, 1232, -1, -5, -2, 242, 100, 4, 423, 2, 564, 9, 0, 10, 43, 64]
+print(array)
+bubble_sort(array)
+print(array)
