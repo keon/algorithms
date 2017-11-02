fb2702a2e524dfeda456bd13de842cab9123e4db
mondeja <mondejar1994@gmail.com>
2017-11-02 20:34:45 +0100

    Circular counter algorithm optimized

--- array/circular_counter.py
+++ array/circular_counter.py
@@ -14,9 +14,9 @@ a = ['1','2','3','4','5','6','7','8','9']
 def josepheus(int_list, skip):
   skip = skip - 1 #list starts with 0 index
   idx = 0
-  len_list(len(int_list))
+  len_list = (len(int_list))
   while len_list>0:
-    idx = (skip+idx)%len(int_list) #hashing to keep changing the index to every 3rd
+    idx = (skip+idx)%len_list #hashing to keep changing the index to every 3rd
     print(int_list.pop(idx))
     len_list -= 1
 
