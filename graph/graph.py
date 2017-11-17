028a083ce7fc7b1cb1333565c420c01f15765971
Thomas Ashish Cherian <6967017+PandaWhoCodes@users.noreply.github.com>
2017-11-17 21:42:08 +0530

    Update graph.py
    With addedge function to add edges to a directed graph

--- graph/graph.py
+++ graph/graph.py
@@ -1,3 +1,25 @@
 class Graph:
-    def __init__(self, node, edges, source):
-        pass
+    def __init__(self, vertices):
+        # No. of vertices
+        self.V = vertices
+
+        # default dictionary to store graph
+        self.graph = {}
+
+        # To store transitive closure
+        self.tc = [[0 for j in range(self.V)] for i in range(self.V)]
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        if u in self.graph:
+            self.graph[u].append(v)
+        else:
+            self.graph[u] = [v]
+            
+g = Graph(4)
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
