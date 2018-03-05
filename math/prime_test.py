b5ba90295653853628ce204e842ed8adce0afbde
Saad <saadbennmushtaq@gmail.com>
2018-03-05 09:57:02 -0600

    Fix syntax error

--- math/prime_test.py
+++ math/prime_test.py
@@ -20,14 +20,14 @@ def prime_test(n):
 
 def prime_test(n):
     # prime numbers are greater than 1
-    if num > 1:
+    if n > 1:
     # check for factors
-        for i in range(2, int(num ** 0.5) + 1):
-           if (num % i) == 0:
+        for i in range(2, int(n ** 0.5) + 1):
+           if (n % i) == 0:
                #print(num,"is not a prime number")
                #print(i,"times",num//i,"is",num)
                 return False
-               break
+                break
         else:
            #print(num,"is a prime number")
             return True
@@ -35,5 +35,4 @@ def prime_test(n):
     # if input number is less than
     # or equal to 1, it is not prime
     else:
-    #print(num,"is not a prime number")
-    return False
+        return False
