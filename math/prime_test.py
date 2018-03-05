820ddf412d09f10977b4bec525d478cc55fe443b
Saad <saadbennmushtaq@gmail.com>
2018-03-04 19:43:35 -0600

    Change the return type to boolean

--- math/prime_test.py
+++ math/prime_test.py
@@ -24,13 +24,16 @@ def prime_test(n):
     # check for factors
         for i in range(2,num):
            if (num % i) == 0:
-               print(num,"is not a prime number")
-               print(i,"times",num//i,"is",num)
+               #print(num,"is not a prime number")
+               #print(i,"times",num//i,"is",num)
+                return False
                break
         else:
-           print(num,"is a prime number")
+           #print(num,"is a prime number")
+            return True
         
     # if input number is less than
     # or equal to 1, it is not prime
     else:
-    print(num,"is not a prime number")
+    #print(num,"is not a prime number")
+    return False
