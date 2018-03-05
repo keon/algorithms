4f6c73253b250ef0b0154a08fabf99974ecbfcb0
Saad <saadbennmushtaq@gmail.com>
2018-03-04 19:37:50 -0600

    Update prime_test.py
    Add a more concise way to check for primality.

--- math/prime_test.py
+++ math/prime_test.py
@@ -16,3 +16,21 @@ def prime_test(n):
             return False
         j += 6
     return True
+
+
+def prime_test(n):
+    # prime numbers are greater than 1
+    if num > 1:
+    # check for factors
+        for i in range(2,num):
+           if (num % i) == 0:
+               print(num,"is not a prime number")
+               print(i,"times",num//i,"is",num)
+               break
+        else:
+           print(num,"is a prime number")
+        
+    # if input number is less than
+    # or equal to 1, it is not prime
+    else:
+    print(num,"is not a prime number")
