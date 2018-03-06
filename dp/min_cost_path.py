ac148205477926a4d861653f1ce6f1fff4b567a2
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-03-06 15:42:42 +0530

    Update min_cost_path.py

--- dp/min_cost_path.py
+++ dp/min_cost_path.py
@@ -9,6 +9,13 @@ station i --> station j   for i < j
 
 NOTE that values where Matrix[i][j] and i > j does not 
 mean anything, and hence represented by -1 or INF
+
+For the input below (cost matrix), 
+Minimum cost is obtained as from  { 0 --> 1 --> 3} 
+                                  = cost[0][1] + cost[1][3] = 65
+the Output will be:
+
+The Minimum cost to reach station 4 is 65
 """ 
 
 INF = float("inf")
