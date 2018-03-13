3491c947b8acc205f548238797f80ab7232c91b0
Rahul Goswami <irg8979@gmail.com>
2018-03-13 15:37:08 +0530

    minor fix

--- dp/regex_matching.py
+++ dp/regex_matching.py
@@ -24,10 +24,10 @@ class Solution(object):
         m, n = len(s) + 1, len(p) + 1
         matches = [[False] * n  for _ in range(m)]
 
-        # Match empty mystring with empty pattern
+        # Match empty string with empty pattern
         matches[0][0] = True
 
-        # Match empty mystring with .*
+        # Match empty string with .*
         for i, element in enumerate(p[1:], 2):
             matches[0][i] = matches[0][i - 2] and element == '*'
 
