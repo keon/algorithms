a1d5099e71da80dc7125818060f0599941714728
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-03-16 14:39:15 +0530

    use snake_case and apces around ` = `

--- graph/Transitive_Closure_DFS.py
+++ graph/Transitive_Closure_DFS.py
@@ -11,15 +11,15 @@ class Graph:
         self.tc = [[0 for j in range(self.V)] for i in range(self.V)]
 
     # function to add an edge to graph
-    def addEdge(self, u, v):
+    def add_edge(self, u, v):
         if u in self.graph:
             self.graph[u].append(v)
         else:
-            self.graph[u]=[v]
+            self.graph[u] = [v]
 
     # A recursive DFS traversal function that finds
     # all reachable vertices for s
-    def DFSUtil(self, s, v):
+    def dfs_util(self, s, v):
 
         # Mark reachability from s to v as true.
         self.tc[s][v] = 1
@@ -27,26 +27,26 @@ class Graph:
         # Find all the vertices reachable through v
         for i in self.graph[v]:
             if self.tc[s][i] == 0:
-                self.DFSUtil(s, i)
+                self.dfs_util(s, i)
 
     # The function to find transitive closure. It uses
-    # recursive DFSUtil()
-    def transitiveClosure(self):
+    # recursive dfs_util()
+    def transitive_closure(self):
 
         # Call the recursive helper function to print DFS
         # traversal starting from all vertices one by one
         for i in range(self.V):
-            self.DFSUtil(i, i)
+            self.dfs_util(i, i)
         print(self.tc)
 
 
 g = Graph(4)
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
 
 print("Transitive closure matrix is")
-g.transitiveClosure()
+g.transitive_closure()
