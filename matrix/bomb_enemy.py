1f03d2f3aa03780971cc23731cf2adb71e7e1dfb
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-03-20 22:28:00 +0530

    enclosed comments in triple quotes

--- matrix/bomb_enemy.py
+++ matrix/bomb_enemy.py
@@ -1,20 +1,21 @@
-# Given a 2D grid, each cell is either a wall 'W',
-# an enemy 'E' or empty '0' (the number zero),
-# return the maximum enemies you can kill using one bomb.
-# The bomb kills all the enemies in the same row and column from
-# the planted point until it hits the wall since the wall is too strong
-# to be destroyed.
-# Note that you can only put the bomb at an empty cell.
+"""
+Given a 2D grid, each cell is either a wall 'W',
+an enemy 'E' or empty '0' (the number zero),
+return the maximum enemies you can kill using one bomb.
+The bomb kills all the enemies in the same row and column from
+the planted point until it hits the wall since the wall is too strong
+to be destroyed.
+Note that you can only put the bomb at an empty cell.
 
-# Example:
-# For the given grid
+Example:
+For the given grid
 
-# 0 E 0 0
-# E 0 W E
-# 0 E 0 0
+0 E 0 0
+E 0 W E
+0 E 0 0
 
-# return 3. (Placing a bomb at (1,1) kills 3 enemies)
-
+return 3. (Placing a bomb at (1,1) kills 3 enemies)
+"""
 
 def max_killed_enemies(grid):
     if not grid: return 0
