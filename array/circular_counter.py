7fede979630a2897322a61f7ecf445a71e6df9b8
Josip Haboić <josiphaboic@gmail.com>
2018-03-26 22:17:32 +0200

    FIX flake8 errors, add test, refactor

--- array/circular_counter.py
+++ array/circular_counter.py
@@ -1,4 +1,6 @@
 """
+https://en.wikipedia.org/wiki/Josephus_problem
+
 There are people sitting in a circular fashion,
 print every third member while removing them,
 the next counter starts immediately after the member is removed.
@@ -7,42 +9,32 @@ Print till all the members are exhausted.
 For example:
 Input: consider 123456789 members sitting in a circular fashion,
 Output: 369485271
+
+
+>>> a = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+>>> josepheus_generator = josepheus(a, 3)
+>>> assert next(josepheus_generator) == '3'
+>>> assert next(josepheus_generator) == '6'
+>>> assert next(josepheus_generator) == '9'
+>>> assert next(josepheus_generator) == '4'
+>>> assert next(josepheus_generator) == '8'
+>>> assert next(josepheus_generator) == '5'
+>>> assert next(josepheus_generator) == '2'
+>>> assert next(josepheus_generator) == '7'
+>>> assert next(josepheus_generator) == '1'
 """
 
-a = ['1','2','3','4','5','6','7','8','9']
 
 def josepheus(int_list, skip):
-  skip = skip - 1 #list starts with 0 index
-  idx = 0
-  len_list = (len(int_list))
-  while len_list>0:
-    idx = (skip+idx)%len_list #hashing to keep changing the index to every 3rd
-    print(int_list.pop(idx))
-    len_list -= 1
+    skip = skip - 1  # list starts with 0 index
+    idx = 0
+    len_list = (len(int_list))
+    while len_list > 0:
+        idx = (skip+idx) % len_list  # hash index to every 3rd
+        yield int_list.pop(idx)
+        len_list -= 1
 
 
-josepheus(a,3)
-
-"""
-the reason for hashing is that we have to find the index of the item which needs to be removed.
-So for e.g. if you iterate with the initial list of folks with every 3rd item eliminated:
-
-INPUT
-int_list = 123456789
-skip = 3
-
-While Iteration:
-
-int_list = 123456789
-len(int_list) = 9
-skip = 2 # as int_list starts from 0
-idx = (0 + 2) % 9 #here previous index was 0
-so 3rd element which is 3 in this case eliminated
-int_list = 12456789
-len(int_list) = 8
-idx = (2 + 2) % 8 #here previous index was 2
-so 3rd element starting from 4th person which is 6 would be deleted.
-and so on
-The reason why we have to do this way is I am not putting the people who escape at the back of list so ideally in 2 while iteration the list should have been
-45678912 and then no hashing needed to be done, which means you can directly remove the third element
-"""
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
--- array/longest_non_repeat.py
+++ array/longest_non_repeat.py
@@ -1,17 +1,25 @@
-# Given a string, find the length of the longest substring
-# without repeating characters.
+"""
+Given a string, find the length of the longest substring
+without repeating characters.
+Examples:
+Given "abcabcbb", the answer is "abc", which the length is 3.
+Given "bbbbb", the answer is "b", with the length of 1.
+Given "pwwkew", the answer is "wke", with the length of 3.
+Note that the answer must be a substring,
+"pwke" is a subsequence and not a substring.
 
-# Examples:
+>>> assert longest_non_repeat("abcabcbb") == 3
+>>> assert longest_non_repeat("bbbbb") == 1
+>>> assert longest_non_repeat("pwwkew") == 3
 
-# Given "abcabcbb", the answer is "abc", which the length is 3.
+"""
 
-# Given "bbbbb", the answer is "b", with the length of 1.
-
-# Given "pwwkew", the answer is "wke", with the length of 3.
-# Note that the answer must be a substring,
-# "pwke" is a subsequence and not a substring.
 
 def longest_non_repeat(string):
+    """
+    Finds the length of the longest substring
+    without repeating characters.
+    """
     if string is None:
         return 0
     temp = []
@@ -23,7 +31,13 @@ def longest_non_repeat(string):
         max_len = max(max_len, len(temp))
     return max_len
 
+
 def longest_non_repeat_two(string):
+    """
+    Finds the length of the longest substring
+    without repeating characters.
+    Uses alternative algorithm.
+    """
     if string is None:
         return 0
     start, max_len = 0, 0
@@ -34,10 +48,9 @@ def longest_non_repeat_two(string):
         else:
             max_len = max(max_len, index - start + 1)
         used_char[char] = index
-    return  max_len
+    return max_len
 
-if __name__ == '__main__':
-    a = "abcabcdefbb"
-    print(a)
-    print(longest_non_repeat(a))
-    print(longest_non_repeat_two(a))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
