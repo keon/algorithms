bcca8b5ce970584624521908ef679fb70beb1f04
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-03-26 23:24:23 +0530

    Update garage.py

--- array/garage.py
+++ array/garage.py
@@ -1,22 +1,23 @@
-# There is a parking lot with only one empty spot. Given the initial state
-# of the parking lot and the final state. Each step we are only allowed to
-# move a car
-# out of its place and move it into the empty spot.
-# The goal is to find out the least movement needed to rearrange
-# the parking lot from the initial state to the final state.
-
-# Say the initial state is an array:
-
-# [1,2,3,0,4],
-# where 1,2,3,4 are different cars, and 0 is the empty spot.
-
-# And the final state is
-
-# [0,3,2,1,4].
-# We can swap 1 with 0 in the initial array to get [0,2,3,1,4] and so on.
-# Each step swap with 0 only.
-# Edited by cyberking-saga
 """
+There is a parking lot with only one empty spot. Given the initial state
+of the parking lot and the final state. Each step we are only allowed to
+move a car
+out of its place and move it into the empty spot.
+The goal is to find out the least movement needed to rearrange
+the parking lot from the initial state to the final state.
+
+Say the initial state is an array:
+
+[1, 2, 3, 0, 4],
+where 1, 2, 3, 4 are different cars, and 0 is the empty spot.
+
+And the final state is
+
+[0, 3, 2, 1, 4].
+We can swap 1 with 0 in the initial array to get [0, 2, 3, 1, 4] and so on.
+Each step swap with 0 only.
+
+Edit:
 Now also prints the sequence of changes in states.
 Output of this example :-
 
@@ -28,7 +29,6 @@ Sequence :
 2 0 3 1 4
 2 3 0 1 4
 0 3 2 1 4
-
 """
 
 
