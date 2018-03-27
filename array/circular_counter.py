2a04d4227f1bcf74ca2fe85ace649d8b6238d900
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-03-27 18:36:59 +0530

    Update circular_counter.py
    added one more test for `StopIteration`.

--- array/circular_counter.py
+++ array/circular_counter.py
@@ -19,22 +19,23 @@ def josepheus(int_list, skip):
         yield int_list.pop(idx)
         len_list -= 1
 
+import unittest
+
+class TestJosepheus(unittest.TestCase):
+    def test_josepheues(self):
+        a = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        josepheus_generator = josepheus(a, 3)
+        self.assertEqual(next(josepheus_generator), '3')
+        self.assertEqual(next(josepheus_generator), '6')
+        self.assertEqual(next(josepheus_generator), '9')
+        self.assertEqual(next(josepheus_generator), '4')
+        self.assertEqual(next(josepheus_generator), '8')
+        self.assertEqual(next(josepheus_generator), '5')
+        self.assertEqual(next(josepheus_generator), '2')
+        self.assertEqual(next(josepheus_generator), '7')
+        self.assertEqual(next(josepheus_generator), '1')
+        self.assertRaises(StopIteration, next, josepheus_generator)
 
 if __name__ == "__main__":
-    import unittest
-
-    class TestJosepheus(unittest.TestCase):
-        def test_josepheues(self):
-            a = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-            josepheus_generator = josepheus(a, 3)
-            self.assertEqual(next(josepheus_generator), '3')
-            self.assertEqual(next(josepheus_generator), '6')
-            self.assertEqual(next(josepheus_generator), '9')
-            self.assertEqual(next(josepheus_generator), '4')
-            self.assertEqual(next(josepheus_generator), '8')
-            self.assertEqual(next(josepheus_generator), '5')
-            self.assertEqual(next(josepheus_generator), '2')
-            self.assertEqual(next(josepheus_generator), '7')
-            self.assertEqual(next(josepheus_generator), '1')
-
+    
     unittest.main()
