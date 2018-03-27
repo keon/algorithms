4b2c85381cfd90191c0224ebb674dd2547a76aa1
Christian Bender <christianbender89@web.de>
2018-03-27 16:04:21 +0200

    added a test suite and renamed one function

--- backtrack/anagram.py
+++ backtrack/anagram.py
@@ -1,4 +1,6 @@
-def all_perms(elements):
+import unittest
+
+def all_perms_iter(elements):
     """
         iterator: returns a perumation by each call. 
     """
@@ -21,9 +23,7 @@ def all_perms(elements):
             for i in range(len(elements)):
                 tmp.append(perm[:i] + elements[0:1] + perm[i:])
         return tmp
-
-word = "abc"
-print(list(all_perms(word)))
+        
 
 def anagram(s1,s2):
     c1 = [0]*26
@@ -37,7 +37,19 @@ def anagram(s1,s2):
         pos = ord(s2[i])-ord('a')
         c2[pos] = c2[pos] + 1
 
-
     return c1 == c2
 
-print(anagram('apple','pleap'))
+
+class TestSuite (unittest.TestCase):
+    def test_all_perms(self):
+        allPerms = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
+        self.assertEqual(allPerms,all_perms("abc"))
+    def test_all_perms_iter(self):
+        it = all_perms_iter("abc")
+        allPerms = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
+        for i in range(len(allPerms)):
+            self.assertEqual(allPerms[i],next(it))
+
+
+if __name__ == "__main__":
+    unittest.main()
