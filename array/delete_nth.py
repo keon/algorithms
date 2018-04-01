d1e1ccb4dcc9905b2a27b7adcc6e7b400b65419b
Saad <saadbennmushtaq@gmail.com>
2018-04-01 09:00:26 -0500

    Update delete.nth.py

--- array/delete_nth.py
+++ array/delete_nth.py
@@ -6,7 +6,7 @@ drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times
 which leads to [1,2,3,1,2,3]
 """
 
-# Time complexity O(n)
+# Time complexity O(n^2)
 def delete_nth(order,max_e):
     ans = []
     for o in order:
@@ -35,8 +35,8 @@ def delete_nth(order, max_e):
     
     
 # Some test cases
-Test.assert_equals(delete_nth([20,37,20,21], 1), [20,37,21], "From list [20,37,20,21],1 you get")
-Test.assert_equals(delete_nth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2], "From list [1,1,3,3,7,2,2,2,2],3 you get ")
-Test.assert_equals(delete_nth([1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1],3), [1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5], "From list [1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1],3 you get ")
-Test.assert_equals(delete_nth([1,1,1,1,1], 5), [1,1,1,1,1], "From list [1,1,1,1,1],5 you get ")
-Test.assert_equals(delete_nth([], 5), [], "From list [],5 you get")
+# Test.assert_equals(delete_nth([20,37,20,21], 1), [20,37,21], "From list [20,37,20,21],1 you get")
+# Test.assert_equals(delete_nth([1,1,3,3,7,2,2,2,2], 3), [1, 1, 3, 3, 7, 2, 2, 2], "From list [1,1,3,3,7,2,2,2,2],3 you get ")
+# Test.assert_equals(delete_nth([1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1],3), [1, 2, 3, 1, 1, 2, 2, 3, 3, 4, 5], "From list [1, 2, 3, 1, 1, 2, 1, 2, 3, 3, 2, 4, 5, 3, 1],3 you get ")
+# Test.assert_equals(delete_nth([1,1,1,1,1], 5), [1,1,1,1,1], "From list [1,1,1,1,1],5 you get ")
+# Test.assert_equals(delete_nth([], 5), [], "From list [],5 you get")
