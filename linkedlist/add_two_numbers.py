cab46bc97eb059e19c3f0ccd71249ce36e10e63d
Rahul Goswami <irg8979@gmail.com>
2018-04-05 17:07:01 +0530

    some fixes and add some tests to linkedlist/

--- linkedlist/add_two_numbers.py
+++ linkedlist/add_two_numbers.py
@@ -13,13 +13,14 @@ Output: 7 -> 0 -> 8
 
 import unittest
 
+
 class Node:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
-def add_two_numbers(left: Node, right: Node )-> Node:
+def add_two_numbers(left: Node, right: Node) -> Node:
     head = Node(0)
     current = head
     sum = 0
@@ -39,7 +40,7 @@ def add_two_numbers(left: Node, right: Node )-> Node:
     return head.next
 
 
-def convert_to_list(number : int) -> Node:
+def convert_to_list(number: int) -> Node:
     """
         converts a positive integer into a (reversed) linked list.
         for example: give 112
@@ -50,7 +51,7 @@ def convert_to_list(number : int) -> Node:
         current = head
         remainder = number % 10
         quotient = number // 10
-        
+
         while quotient != 0:
             current.next = Node(remainder)
             current = current.next
@@ -62,7 +63,7 @@ def convert_to_list(number : int) -> Node:
         print("number must be positive!")
 
 
-def convert_to_str(l : Node) -> str:
+def convert_to_str(l: Node) -> str:
     """
         converts the non-negative number list into a string.
     """
@@ -73,16 +74,18 @@ def convert_to_str(l : Node) -> str:
     return result
 
 
-class Testsuite(unittest.TestCase):
+class TestSuite(unittest.TestCase):
     """
         testsuite for the linked list structure and
         the adding function, above.
     """
+
     def test_convert_to_str(self):
         number1 = Node(2)
         number1.next = Node(4)
         number1.next.next = Node(3)
-        self.assertEqual("243",convert_to_str(number1))
+        self.assertEqual("243", convert_to_str(number1))
+
     def test_add_two_numbers(self):
         # 1. test case
         number1 = Node(2)
@@ -91,9 +94,9 @@ class Testsuite(unittest.TestCase):
         number2 = Node(5)
         number2.next = Node(6)
         number2.next.next = Node(4)
-        result = convert_to_str(add_two_numbers(number1,number2))
-        self.assertEqual("708",result)
-        
+        result = convert_to_str(add_two_numbers(number1, number2))
+        self.assertEqual("708", result)
+
         # 2. test case
         number3 = Node(1)
         number3.next = Node(1)
@@ -101,15 +104,15 @@ class Testsuite(unittest.TestCase):
         number4 = Node(1)
         number4.next = Node(0)
         number4.next.next = Node(1)
-        result = convert_to_str(add_two_numbers(number3,number4))
-        self.assertEqual("2101",result)
-        
+        result = convert_to_str(add_two_numbers(number3, number4))
+        self.assertEqual("2101", result)
+
         # 3. test case
         number5 = Node(1)
         number6 = Node(0)
-        result = convert_to_str(add_two_numbers(number5,number6))
-        self.assertEqual("1",result)
-        
+        result = convert_to_str(add_two_numbers(number5, number6))
+        self.assertEqual("1", result)
+
         # 4. test case
         number7 = Node(9)
         number7.next = Node(1)
@@ -117,13 +120,13 @@ class Testsuite(unittest.TestCase):
         number8 = Node(1)
         number8.next = Node(0)
         number8.next.next = Node(1)
-        result = convert_to_str(add_two_numbers(number7,number8))
-        self.assertEqual("022",result)
+        result = convert_to_str(add_two_numbers(number7, number8))
+        self.assertEqual("022", result)
+
     def test_convert_to_list(self):
         result = convert_to_str(convert_to_list(112))
-        self.assertEqual("211",result)
-        
+        self.assertEqual("211", result)
+
 
 if __name__ == "__main__":
     unittest.main()
-    
--- linkedlist/intersection.py
+++ linkedlist/intersection.py
@@ -1,15 +1,19 @@
+"""
+   This function takes two lists and returns the node they have in common, if any.
+   In this example:
+   1 -> 3 -> 5
+               \
+                7 -> 9 -> 11
+               /
+   2 -> 4 -> 6
+   ...we would return 7.
+   Note that the node itself is the unique identifier, not the value of the node.
+   """
+import unittest
+
+
 def intersection(h1, h2):
-    """
-    This function takes two lists and returns the node they have in common, if any.
-    In this example:
-    1 -> 3 -> 5
-                \
-                 7 -> 9 -> 11
-                /
-    2 -> 4 -> 6
-    ...we would return 7.
-    Note that the node itself is the unique identifier, not the value of the node.
-    """
+
     count = 0
     flag = None
     h1_orig = h1
