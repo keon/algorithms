6deede63d5659942392f9754af8b76246a3c2669
Christian Bender <christianbender89@web.de>
2018-04-08 15:17:33 +0200

    fixed insertion_sort.py.

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -2,7 +2,7 @@ def insertion_sort(arr):
     """ Insertion Sort
         Complexity: O(n^2)
     """
-    for i in xrange(len(arr)):
+    for i in range(len(arr)):
         cursor = arr[i]
         pos = i
         while pos > 0 and arr[pos-1] > cursor:
