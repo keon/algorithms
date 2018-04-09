675211f3de5a481a3435dea3ec3f9c46298fbc32
tratneshsingh <tratneshsingh@gmail.com>
2018-04-08 21:56:20 -0700

    Update topsort.py
    Update topsort.py
    
    Correcting the toposort, the earlier code was giving incorrect value for 
    depGraph = {
        "a" : [ "b" ],
        "b" : [ "c" ],
        "c" : [ "d" ],
        "d" : []
    }
    given = [ "a", "b", "c", "d"]
    Fixed the review comment of passing  G to the recursive routine.

--- sort/topsort.py
+++ sort/topsort.py
@@ -23,36 +23,21 @@ depGraph = {
 
 given = [ "b", "c", "a", "d", "e", "f" ]
 
-def retDeps(visited, start):
-    queue = []
-    out = []
-    queue.append(start)
-    while queue:
-        newNode = queue.pop(0)
-        if newNode not in visited:
-            visited.add(newNode)
-        for child in depGraph[newNode]:
-            queue.append(child)
-            out.append(child)
+given = [ "b", "c", "a", "d", "e", "f" ]
+def retDeps(G, out, visited, start):
+    if start in visited:
+      return
+    visited.add(start)
+    for child in G[start]:
+      retDeps(G, out, visited, child)
     out.append(start)
-    return out
 
 
-def retDepGraph():
+def retDepGraph(G):
     visited = set()
     out = []
-    # visited.add(given[0])
     for pac in given:
-        if pac in visited:
-            continue
-        visited.add(pac)
-        #out.append(pac)
-        if pac in depGraph:
-            # find all children
-            for child in depGraph[pac]:
-                if child in visited:
-                    continue
-                out.extend(retDeps(visited, child))
-        out.append(pac)
+      retDeps(G, out, visited, pac)
     print(out)
-retDepGraph()
+  
+retDepGraph(depGraph)
