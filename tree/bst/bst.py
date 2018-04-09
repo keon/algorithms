a0037cdd7fbacdc1f4e594ea297234935da69e2f
tratneshsingh <tratneshsingh@gmail.com>
2018-04-09 06:30:11 -0700

    Update bst.py
    Removing redundant checks moving root is None check in recur_search/recur_size.
    Similar change could be done in recur_insert.
    ```python
    def insert(self,data):
        self.root = self.recur_insert(self.root, data)
    
    def recur_insert(self, root, data):
        if root is None:
            return Node(data)
        if root.data == data:
            return # Do Nothing
        elif data < root.data:
            root.left = self.recur_insert(root.left, data)
        else:
            root.right = self.recur_insert(root.right, data)
    ```

--- tree/bst/bst.py
+++ tree/bst/bst.py
@@ -31,10 +31,6 @@ class bst(object):
     def recur_size(self, root):
         if root is None:
             return 0
-        elif root.left is None:
-            return 1 + self.recur_size(root.right)
-        elif root.right is None:
-            return 1 + self.recur_size(root.left)
         else:
             return 1 + self.recur_size(root.left) + self.recur_size(root.right)
 
@@ -43,16 +39,13 @@ class bst(object):
         Using recursion. Complexity O(logN)
     """
     def search(self, data):
-        if self.root:
-            return self.recur_search(self.root, data)
-        else:
-            return False
+        return self.recur_search(self.root, data)
 
     def recur_search(self, root, data):
+        if root is None:
+            return False
         if root.data == data:
             return True
-        elif root.left is None and root.right is None:
-            return False
         elif data > root.data:     # Go to right root
             return self.recur_search(root.right, data)
         else:                      # Go to left root
