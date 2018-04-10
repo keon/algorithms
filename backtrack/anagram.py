92b361233810723fa60c22d99a28b9a79d2ded33
Rahul Goswami <irg8979@gmail.com>
2018-04-11 00:32:40 +0530

    some refactor

--- backtrack/anagram.py
+++ backtrack/anagram.py
@@ -11,6 +11,7 @@ def all_perms_iter(elements):
             for i in range(len(elements)):
                 yield perm[:i] + elements[0:1] + perm[i:]
 
+
 def all_perms(elements):
     """
         returns a list with the permuations.
@@ -24,26 +25,34 @@ def all_perms(elements):
                 tmp.append(perm[:i] + elements[0:1] + perm[i:])
         return tmp
 
-def anagram(s1,s2):
-    c1 = [0]*26
-    c2 = [0]*26
-    for i in range(len(s1)):
-        pos = ord(s1[i])-ord('a')
+
+def anagram(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+
+    for c in s1:
+        pos = ord(c)-ord('a')
         c1[pos] = c1[pos] + 1
-    for i in range(len(s2)):
-        pos = ord(s2[i])-ord('a')
+
+    for c in s2:
+        pos = ord(c)-ord('a')
         c2[pos] = c2[pos] + 1
+
     return c1 == c2
 
+
 class TestSuite (unittest.TestCase):
+
     def test_all_perms(self):
         perms = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
         self.assertEqual(perms, all_perms("abc"))
+
     def test_all_perms_iter(self):
         it = all_perms_iter("abc")
         perms = ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
         for i in range(len(perms)):
             self.assertEqual(perms[i], next(it))
+
     def test_angram(self):
         self.assertTrue(anagram('apple', 'pleap'))
         self.assertFalse(anagram("apple", "cherry"))
--- backtrack/factor_combinations.py
+++ backtrack/factor_combinations.py
@@ -34,22 +34,23 @@ output:
 ]
 """
 
-# Iterative:
 
-def getFactors(self, n):
+# Iterative:
+def get_factors(n):
     todo, combis = [(n, 2, [])], []
     while todo:
         n, i, combi = todo.pop()
         while i * i <= n:
             if n % i == 0:
-                combis.append(combi + [i, n//i]),
-                todo.append( [n // i, i, combi+[i] ] )  # python3: n // i
+                combis.append(combi + [i, n//i])
+                todo.append((n//i, i, combi+[i]))
             i += 1
     return combis
 
-# Recursive:
 
-def getFactors(self, n):
+# Recursive:
+def get_factors_recur(n):
+
     def factor(n, i, combi, combis):
         while i * i <= n:
             if n % i == 0:
@@ -57,4 +58,5 @@ def getFactors(self, n):
                 factor(n//i, i, combi+[i], combis)
             i += 1
         return combis
+
     return factor(n, 2, [], [])
