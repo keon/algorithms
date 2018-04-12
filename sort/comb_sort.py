d621c53cb28fd38e1583012067ad0ef8e7c726f7
danghai <danghai@pdx.edu>
2018-04-12 16:34:48 -0700

    Separate test for comb_sort

--- sort/comb_sort.py
+++ sort/comb_sort.py
@@ -33,13 +33,3 @@ def comb_sort(arr):
                 sorted = False
             i = i + 1
     return arr
-
-class TestSuite(unittest.TestCase):
-    """
-        test suite for the function (above)
-    """
-    def test_comb_sort(self):
-        self.assertEqual([1, 5, 23, 57, 65, 1232],
-                         comb_sort([1, 5, 65, 23, 57, 1232]))
-if __name__ == "__main__":
-    unittest.main()
--- sort/test_sort.py
+++ sort/test_sort.py
@@ -1,4 +1,5 @@
 from bubble_sort import bubble_sort
+from comb_sort import comb_sort
 
 import unittest
 
@@ -6,6 +7,10 @@ class TestSuite(unittest.TestCase):
         def test_bubble_sort(self):
             self.assertEqual([1, 5, 23, 57, 65, 1232],
                              bubble_sort([1, 5, 65, 23, 57, 1232]))
+
+        def test_comb_sort(self):
+            self.assertEqual([1, 5, 23, 57, 65, 1232],
+                             comb_sort([1, 5, 65, 23, 57, 1232]))
 
 if __name__ == "__main__":
     unittest.main()
