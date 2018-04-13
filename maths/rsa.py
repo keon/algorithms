73ab539aa1c0bc6c9b02a5a8ccb0fcbf7c53a098
Rahul Goswami <irg8979@gmail.com>
2018-04-13 21:51:26 +0530

    added seed in maths/rsa.py and pep8 fixes in test_maths.py

--- maths/rsa.py
+++ maths/rsa.py
@@ -23,66 +23,72 @@ a number very quickly:
 
 import random
 
-from rabin_miller import * # is_prime
-from extended_gcd import * # extended_gcd
 
-"""
-generate a prime with k bits
-"""
-def genprime(k):
-    random.seed()
-    while True:
-        n = random.randrange(int(2 ** (k - 1)),int(2 ** k))
-        if is_prime(n,128):
-            return n
-
-
-"""
-calculate the inverse of a mod m
-that is, find b such that (a * b) % m == 1
-"""
 def modinv(a, m):
-        b = 1
-        while ((a*b) % m != 1):
-            b += 1
-        return b
+    """calculate the inverse of a mod m
+    that is, find b such that (a * b) % m == 1"""
+    b = 1
+    while not (a * b) % m == 1:
+        b += 1
+    return b
 
-"""
-the RSA key generating algorithm
-k is the number of bits in n
-"""
-def generate_key(k):
+
+def generate_key(k, seed=None):
+    """
+    the RSA key generating algorithm
+    k is the number of bits in n
+    """
+
+    def gen_prime(k, seed=None):
+        """generate a prime with k bits"""
+
+        def is_prime(num):
+            if num == 2:
+                return True
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+
+        random.seed(seed)
+        while True:
+            key = random.randrange(int(2 ** (k - 1)), int(2 ** k))
+            if is_prime(key):
+                return key
+
     # size in bits of p and q need to add up to the size of n
     p_size = k / 2
     q_size = k - p_size
     
-    e = genprime(k) # in many cases, e is also chosen to be a small constant
+    e = gen_prime(k, seed)  # in many cases, e is also chosen to be a small constant
     
     while True:
-        p = genprime(k / 2)
+        p = gen_prime(p_size, seed)
         if p % e != 1:
             break
     
     while True:
-        q = genprime(k - k / 2)
+        q = gen_prime(q_size, seed)
         if q % e != 1:
             break
     
     n = p * q
-    l = (p - 1) * (q - 1) # calculate totient function
-    d = modinv(e,l)
+    l = (p - 1) * (q - 1)  # calculate totient function
+    d = modinv(e, l)
     
     return int(n), int(e), int(d)
 
+
 def encrypt(data, e, n):
     return pow(int(data), int(e), int(n))
+
 
 def decrypt(data, d, n):
     return pow(int(data), int(d), int(n))
 
 
 
-#sample usage:
+# sample usage:
 # n,e,d = generate_key(16)
 # data = 20
 # encrypted = pow(data,e,n)
