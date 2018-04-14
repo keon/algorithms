94241fc42bfd051acb6eb9e474007b93ed4564a7
Murilo Camargos <murilo@ckl.io>
2018-04-14 12:58:57 -0300

    Comment out testing lines

--- graph/graph.py
+++ graph/graph.py
@@ -82,10 +82,10 @@ class Graph:
         else:
             self.graph[u] = [v]
             
-g = Graph(4)
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 0)
-g.add_edge(2, 3)
-g.add_edge(3, 3)
+#g = Graph(4)
+#g.add_edge(0, 1)
+#g.add_edge(0, 2)
+#g.add_edge(1, 2)
+#g.add_edge(2, 0)
+#g.add_edge(2, 3)
+#g.add_edge(3, 3)
