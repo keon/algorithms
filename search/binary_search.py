6e745021a2c918464a3dbf5545f01cb03c6eda26
danghai <danghai@pdx.edu>
2018-04-15 16:28:19 -0700

    Separate test for search

--- search/binary_search.py
+++ search/binary_search.py
@@ -4,8 +4,6 @@
 #       increasing order.
 # T(n): O(log n)
 #
-
-
 def binary_search(array, query):
     lo, hi = 0, len(array) - 1
     while lo <= hi:
@@ -18,20 +16,3 @@ def binary_search(array, query):
         else:
             hi = mid - 1
     return None
-
-
-def main():
-    array = [1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6]
-    print(array)
-    print("-----SEARCH-----")
-    print("found: ", 5, " in index:", binary_search(array, 5))
-    print("-----SEARCH-----")
-    print("found: ", 6, " in index:", binary_search(array, 6))
-    print("-----SEARCH-----")
-    print("found: ", 7, " in index:", binary_search(array, 7))
-    print("-----SEARCH-----")
-    print("found: ", -1, " in index:", binary_search(array, -1))
-    print("-----SEARCH-----")
-
-if __name__ == "__main__":
-    main()
---
+++ search/test_search.py
@@ -0,0 +1,38 @@
+from binary_search import binary_search
+from first_occurance import first_occurance
+from last_occurance import last_occurance
+
+import unittest
+
+class TestSuite(unittest.TestCase):
+
+    def test_first_occurance(self):
+        def helper(array, query):
+            idx = array.index(query) if query in array else None
+            return idx
+        array = [1, 1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6]
+        self.assertEqual(first_occurance(array, 1), helper(array, 1))
+        self.assertEqual(first_occurance(array, 3), helper(array, 3))
+        self.assertEqual(first_occurance(array, 5), helper(array, 5))
+        self.assertEqual(first_occurance(array, 6), helper(array, 6))
+        self.assertEqual(first_occurance(array, 7), helper(array, 7))
+        self.assertEqual(first_occurance(array, -1), helper(array, -1))
+
+    def test_binary_search(self):
+        array = [1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6]
+        self.assertEqual(10, binary_search(array, 5))
+        self.assertEqual(11, binary_search(array, 6))
+        self.assertEqual(None, binary_search(array, 7))
+        self.assertEqual(None, binary_search(array, -1))
+
+    def test_last_occurance(self):
+        array = [1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6]
+        self.assertEqual(5, last_occurance(array, 3))
+        self.assertEqual(10, last_occurance(array, 5))
+        self.assertEqual(None, last_occurance(array, 7))
+        self.assertEqual(0, last_occurance(array, 1))
+        self.assertEqual(13, last_occurance(array, 6))
+
+if __name__ == '__main__':
+
+    unittest.main()
