317677c964948b8064c0c43ed85a9b0fc8365ef5
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-04-22 23:19:38 +0530

    fix the error in is_palindrome

--- strings/is_palindrome.py
+++ strings/is_palindrome.py
@@ -1,15 +1,12 @@
 """
 Given a string, determine if it is a palindrome,
 considering only alphanumeric characters and ignoring cases.
-
 For example,
 "A man, a plan, a canal: Panama" is a palindrome.
 "race a car" is not a palindrome.
-
 Note:
 Have you consider that the string might be empty?
 This is a good question to ask during an interview.
-
 For the purpose of this problem,
 we define empty string as valid palindrome.
 """
@@ -35,23 +32,19 @@ def is_palindrome(s):
 
 """
 Here is a bunch of other variations of is_palindrome function.
-
 Variation 1:
 Find the reverse of the string and compare it with the original string
-
 Variation 2:
 Loop from the start to length/2 and check the first character and last character
 and so on... for instance s[0] compared with s[n-1], s[1] == s[n-2]...
-
 Variation 3:
 Using stack idea. 
 Note: We are assuming that we are just checking a one word string. To check if a complete sentence 
-
 """    
 def remove_punctuation(s):
-	"""
-	Remove punctuation, case sensitivity and spaces
-	"""
+    """
+    Remove punctuation, case sensitivity and spaces
+    """
     return "".join(i.lower() for i in s if i in string.ascii_letters)
 
 # Variation 1
@@ -70,26 +63,22 @@ def is_palidrome_reverse(s):
 
 # Variation 2
 def is_palidrome_two_pointer(s):
-	s = remove_punctuation(s)
+    s = remove_punctuation(s)
 	
-	for i in range(0 : len(s)/2):
-		if (s[i] != s[len(s) - i - 1]):
-			return False
-	return True
+    for i in range(0, len(s)//2):
+        if (s[i] != s[len(s) - i - 1]):
+            return False
+    return True
 	
-	for i in range(0, len(s)//2):
-		if (s[i] != s[len(s) - i - 1]):
-			return False
-	return True
-	
+
 # Variation 3
 def is_palindrome_stack(s):
-	stack = []
-	s = remove_punctuation(s)
+    stack = []
+    s = remove_punctuation(s)
 	
-	for i in range(len(s)//2, len(s)):
-		stack.append(s[i])
-	for i in range(0, len(s)//2):
-		if s[i] != stack.pop():
-			return False
-	return True	
+    for i in range(len(s)//2, len(s)):
+        stack.append(s[i])
+    for i in range(0, len(s)//2):
+        if s[i] != stack.pop():
+            return False
+    return True	
