d267b2a7a684c1fa510b5a75b33e402c9f786742
Christian Bender <christianbender89@web.de>
2018-04-24 14:21:41 +0200

    changed formatting

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -2,6 +2,7 @@ from strings.add_binary import add_binary
 
 import unittest
 
+
 class TestAddBinary(unittest.TestCase):
     """[summary]
     Test for the file add_binary.py
@@ -9,11 +10,12 @@ class TestAddBinary(unittest.TestCase):
     Arguments:
         unittest {[type]} -- [description]
     """
-    
+
     def test_add_binary(self):
         self.assertEqual("100",add_binary("11","1"))
         self.assertEqual("101",add_binary("100","1"))
         self.assertEqual("10",add_binary("1","1"))
+
 
 if __name__ == "__main__":
     unittest.main()
