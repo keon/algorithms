f0f3f334e1d473cd10f29a23b442977e6c99b59b
Christian Bender <christianbender89@web.de>
2018-04-24 21:09:11 +0200

    remove inline test

--- strings/strip_url_params.py
+++ strings/strip_url_params.py
@@ -94,25 +94,3 @@ def strip_url_params3(url, strip=None):
     new = parse._replace(query=query)
     
     return new.geturl()
-
-
-class TestSuite(unittest.TestCase):
-
-    def test_strip_url_params1(self):
-    
-        self.assertEqual(strip_url_params1("www.saadbenn.com?a=1&b=2&a=2"), "www.saadbenn.com?a=1&b=2")
-        self.assertEqual(strip_url_params1("www.saadbenn.com?a=1&b=2", ['b']), "www.saadbenn.com?a=1")
-
-    
-    def test_strip_url_params2(self):
-    
-        self.assertEqual(strip_url_params2("www.saadbenn.com?a=1&b=2&a=2"), "www.saadbenn.com?a=1&b=2")
-        self.assertEqual(strip_url_params2("www.saadbenn.com?a=1&b=2", ['b']), "www.saadbenn.com?a=1")
-    
-    def test_strip_url_params3(self):
-    
-        self.assertEqual(strip_url_params3("www.saadbenn.com?a=1&b=2&a=2"), "www.saadbenn.com?a=1&b=2")
-        self.assertEqual(strip_url_params3("www.saadbenn.com?a=1&b=2", ['b']), "www.saadbenn.com?a=1")
-
-if __name__ == "__main__":
-    unittest.main()
