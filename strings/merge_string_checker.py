0d06bdaf4f188c532d37319e77bd2b1f74ae54e9
Christian Bender <christianbender89@web.de>
2018-04-24 20:05:06 +0200

    changed the description

--- strings/merge_string_checker.py
+++ strings/merge_string_checker.py
@@ -1,7 +1,9 @@
 """
-At a job interview, you are challenged to write an algorithm to check if a given string, s, can be formed from two other strings, part1 and part2.
-The restriction is that the characters in part1 and part2 are in the same order as in s.
-The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+At a job interview, you are challenged to write an algorithm to check if a 
+given string, s, can be formed from two other strings, part1 and part2.
+The restriction is that the characters in part1 and part2 are in the same 
+order as in s. The interviewer gives you the following example and tells 
+you to figure out the rest from the given test cases.
 'codewars' is a merge from 'cdw' and 'oears':
 s:  c o d e w a r s   = codewars
 part1:  c   d   w         = cdw
