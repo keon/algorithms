0cd9e2cbd59fe761d7270bf4d1e8b91124cda813
njsao <sjnao89@gmail.com>
2018-04-26 00:05:57 +0200

    added first tests for the tree.bst module and some refractoring

--- tree/bst/depth_sum.py
+++ tree/bst/depth_sum.py
@@ -15,11 +15,8 @@ For example:
     depth_sum = 1*9 + 2*(6+12) + 3*(3+8+10+15) + 4*(7+18)
 
 """
-import unittest
-from bst import Node
-from bst import bst
 
-def depth_sum(root, n):
+def depth_sum(root):
     if root:
         return recur_depth_sum(root, 1)
 
@@ -46,21 +43,6 @@ def recur_depth_sum(root, n):
 
 """
 
-class TestSuite(unittest.TestCase):
-    def setUp(self):
-        self.tree = bst()
-        self.tree.insert(9)
-        self.tree.insert(6)
-        self.tree.insert(12)
-        self.tree.insert(3)
-        self.tree.insert(8)
-        self.tree.insert(10)
-        self.tree.insert(15)
-        self.tree.insert(7)
-        self.tree.insert(18)
 
-    def test_depth_sum(self):
-        self.assertEqual(253, depth_sum(self.tree.root, 4))
 
-if __name__ == '__main__':
-    unittest.main()
+
