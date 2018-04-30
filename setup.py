c6976b699a0637f921dfb0c391c8ac909a26d5d1
danghai <danghai@pdx.edu>
2018-04-29 19:13:59 -0700

    Add check install and uninstall package to Travis

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 setup(name='algorithms',
-      version='1.0',
+      version='1.1',
       description='Pythonic Data Structures and Algorithms',
       url='https://github.com/keon/algorithms',
       author='Keon Kim, Hai Hoang Dang, Rahul Goswami, Christian Bender, Saad',
