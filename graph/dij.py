70de42468241780ed3aa931d7eaa3a3817a24aab
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-05-02 03:59:36 +0000

    Update dij.py
    provided test case

--- graph/dij.py
+++ graph/dij.py
@@ -3,20 +3,20 @@
 import sys
  
 class Graph():
- 
+
     def __init__(self, vertices):
         self.V = vertices
         self.graph = [[0 for column in range(vertices)] 
                       for row in range(vertices)]
  
     def printSolution(self, dist):
-        print("Vertex \t\t Distance from Source")
+        print("Vertex \t\t Min Distance from Source")
         for node in range(self.V):
             print(node,"   \t\t",dist[node])
  
 
     def minDistance(self, dist, MinSet):
-        min = sys.maxint
+        min = sys.maxsize
         for v in range(self.V):
             if dist[v] < min and MinSet[v] == False:
                 min = dist[v]
@@ -27,7 +27,7 @@ class Graph():
 
     def dijkstra(self, src):
  
-        dist = [sys.maxint] * self.V
+        dist = [sys.maxsize] * self.V
         dist[src] = 0
         MinSet = [False] * self.V
  
@@ -47,12 +47,20 @@ class Graph():
         self.printSolution(dist)
  
 #MAIN
-vert=input("Enter the number of vertices: ")
+"""vert=input("Enter the number of vertices: ")
 g  = Graph(vert)
 print("Enter the adjacency matrix representation of graph: ")
 for i in range(0, 9):
 	for j in range(0, 9):
 		g.graph[i][j]=input()
 sv=input("Enter the source vertex: ")
-g.dijkstra(sv);
- 
+g.dijkstra(sv);"""
+
+g=Graph(6)
+g.graph= [[0, 4, 4, 0, 0, 0],
+	  [4, 0, 2, 0, 0, 0],
+	  [4, 2, 0, 3, 6, 1],
+	  [0, 0, 3, 0, 2, 0],
+	  [0, 0, 6, 2, 0, 3],
+	  [0, 0, 1, 0, 3, 0]]
+g.dijkstra(0)
