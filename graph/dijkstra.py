e41b0ee0436afe9c4b78bc3adbfa995371cfe9e7
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-05-03 14:16:25 +0530

    Update dijkstra.py
    corrected indentation

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -4,18 +4,15 @@ class Graph():
 
     def __init__(self, vertices):
         self.vertices = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
 
     def min_distance(self, dist, min_dist_set):
-        min = float("inf")
+        min_dist = float("inf")
         for v in range(self.vertices):
-            if dist[v] < min and min_dist_set[v] == False:
-                min = dist[v]
+            if dist[v] < min_dist and min_dist_set[v] == False:
+                min_dist = dist[v]
                 min_index = v
-
         return min_index
-
 
     def dijkstra(self, src):
 
@@ -23,15 +20,17 @@ class Graph():
         dist[src] = 0
         min_dist_set = [False] * self.vertices
 
-	for cout in range(self.vertices):
-	#minimum distance vertex that is not processed
-		u = self.min_distance(dist, min_dist_set)
+        for count in range(self.vertices):
 
- 	#put minimum distance vertex in shortest tree
-		min_dist_set[u] = True
+            #minimum distance vertex that is not processed
+            u = self.min_distance(dist, min_dist_set)
 
-	# Update dist value of the adjacent vertices
-		for v in range(self.vertices):
-			if self.graph[u][v] > 0 and min_dist_set[v] == False and dist[v] > dist[u] + self.graph[u][v]:
-				dist[v] = dist[u] + self.graph[u][v]
-	return dist
+            #put minimum distance vertex in shortest tree
+            min_dist_set[u] = True
+
+            #Update dist value of the adjacent vertices
+            for v in range(self.vertices):
+                if self.graph[u][v] > 0 and min_dist_set[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+
+        return dist
