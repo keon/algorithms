a3fee83a96d76b2a377848cf7c6855ce0ab98ed6
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-05-03 14:29:33 +0530

    Update test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -133,7 +133,7 @@ class TestStack(unittest.TestCase):
 class TestOrderedStack(unittest.TestCase):
     def test_OrderedStack(self):
         stack = OrderedStack()
-        self.assertTrue(stack.isEmpty())
+        self.assertTrue(stack.is_empty())
         stack.push(1)
         stack.push(4)
         stack.push(3)
