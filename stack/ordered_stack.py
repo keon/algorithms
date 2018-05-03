547087db715dd35daea91e0e1a765ce487546ce2
Agranya Singh <35340618+agranya99@users.noreply.github.com>
2018-05-03 14:24:14 +0530

    Update ordered_stack.py
    Forgot to remove return statement from push function. I needed it in one of my programs. Removed. Removed redundant brackets

--- stack/ordered_stack.py
+++ stack/ordered_stack.py
@@ -4,27 +4,25 @@ class OrderedStack:
      def __init__(self):
          self.items = []
 
-     def isEmpty(self):
+     def is_empty(self):
          return self.items == []
 
-     def pushT(self, item):
+     def push_t(self, item):
          self.items.append(item)
 
      def push(self, item): #push method to maintain order when pushing new elements
-         temp = OrderedStack()
-         if self.isEmpty() or item > self.peek():
-             self.pushT(item)
-             return self.items
+         temp_stack = OrderedStack()
+         if self.is_empty() or item > self.peek():
+             self.push_t(item)
          else:
-             while (item < self.peek()) and (not self.isEmpty()):
-                 temp.pushT(self.pop())
-             self.pushT(item)
-             while not(temp.isEmpty()):
-                 self.pushT(temp.pop())
-             return self.items
+             while item < self.peek() and not self.is_empty():
+                 temp_stack.push_t(self.pop())
+             self.push_t(item)
+             while not temp_stack.is_empty():
+                 self.push_t(temp_stack.pop())
 
      def pop(self):
-         if self.isEmpty():
+         if self.is_empty():
              raise IndexError("Stack is empty")
          return self.items.pop()
 
