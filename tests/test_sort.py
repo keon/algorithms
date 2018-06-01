68d2c4bb344946576343b4176c587b5d38aaf8e7
Lee Geon <lg970325@naver.com>
2018-06-01 11:45:15 +0900

    Update test_sort.py

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -6,6 +6,8 @@ from sort.insertion_sort import insertion_sort
 from sort.merge_sort import merge_sort
 from sort.quick_sort import quick_sort
 from sort.selection_sort import selection_sort
+from sort.bucket_sort import bucket_sort
+from sort.shell_sort import shell_sort
 
 import unittest
 
