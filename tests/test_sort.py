863c05441c4a3a740c6bac378be007b995b6efae
Han SangWook <9967han@naver.com>
2018-06-06 00:12:30 +0900

    Update test_sort.py

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -19,7 +19,7 @@ import unittest
 class TestSuite(unittest.TestCase):
     def test_bogo_sort(self):
         self.assertEqual([1, 5, 23, 57, 65, 1232],
-                         bubble_sort([1, 5, 65, 23, 57, 1232]))
+                         bogo_sort([1, 5, 65, 23, 57, 1232]))
    
     def test_bubble_sort(self):
         self.assertEqual([1, 5, 23, 57, 65, 1232],
