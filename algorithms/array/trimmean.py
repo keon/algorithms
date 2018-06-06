c7152155972e0366bd4a321360f8548a4ca06d8b
aig031 <31883686+aig031@users.noreply.github.com>
2018-06-06 21:19:10 +0900

    Update trimmean.py

--- algorithms/array/trimmean.py
+++ algorithms/array/trimmean.py
@@ -2,6 +2,11 @@
 When make reliable means, we need to neglect best and worst value. For example, when making average score on athletes we need this option.
 So, this algorithms, fix some percentage to neglect when making mean. For example, if you suggest 20%, it will neglect best 10% value, and
 worst 10% value.
+
+This algorithm gets array and percentage to neglect. After sorted, if index of array is larger or smaller or wanted ratio, we don't
+compute it.
+
+Compleity: O(n)
 """
 def trimmean(arr, per):
     ratio = per/200
@@ -9,11 +14,9 @@ def trimmean(arr, per):
     cal_sum = 0
     # sum value to be calculated to trimmean.
     arr.sort()
-    count = 0
-    # count how many values added.
-    for i in range(len(arr)):
-        if i>(len(arr)*ratio-1) and i<(len(arr)-len(arr)*ratio):
-            cal_sum += arr[i]
-            count += 1
-    #print(cal_sum, count)
-    return cal_sum/count
+    neg_val = int(len(arr)*ratio)
+    arr = arr[neg_val:len(arr)-neg_val]
+    for i in arr:
+        cal_sum += i
+    #print(cal_sum, len(arr))
+    return cal_sum/len(arr)
