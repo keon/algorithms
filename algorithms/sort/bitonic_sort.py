9ad887dff85801e2024e80063f3d8c736f7c60df
hsi1032 <hsi1032@naver.com>
2018-06-06 21:58:15 +0900

    Update bitonic_sort.py

--- algorithms/sort/bitonic_sort.py
+++ algorithms/sort/bitonic_sort.py
@@ -28,14 +28,14 @@ def bitonic_sort(arr, reverse=False):
         return left + right
     
     #end of function(compare and bitionic_merge) definition
+    n = len(arr)
+    
     # checks if n is power of two
     if (n and (not(n & (n - 1))) ):
         raise ValueError("the size of input should be power of two")
     
-    n = len(arr)
     if n <= 1:
         return arr
-    
     left = bitonic_sort(arr[:n // 2], True)
     right = bitonic_sort(arr[n // 2:], False)
 
