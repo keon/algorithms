8ff4ab3ac1ef8c6a48a617b3e838804153bd0753
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-06-06 15:06:12 +0530

    Update gnome_sort.py

--- algorithms/sort/gnome_sort.py
+++ algorithms/sort/gnome_sort.py
@@ -12,8 +12,8 @@ def gnome_sort(arr):
     index = 0
     while index < n:
         if index == 0 or arr[index] >= arr[index-1]:
-            index = index +1
+            index = index + 1
         else:
             arr[index], arr[index-1] = arr[index-1], arr[index]
-            index = index -1
+            index = index - 1
     return arr
