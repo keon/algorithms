102f06b98e059cbb39ee0e96558fa66d09232cb8
Lee Geon <lg970325@naver.com>
2018-06-07 11:10:32 +0900

    Update bubble_sort.py

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -7,16 +7,22 @@ Worst-case performance: O(N^2)
 """
 
 
-def bubble_sort(arr):
+def bubble_sort(arr,simulation=False):
     def swap(i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
     n = len(arr)
     swapped = True
+    
+    if simulation == True:
+        print arr
+    
     while swapped:
         swapped = False
         for i in range(1, n):
             if arr[i - 1] > arr[i]:
                 swap(i - 1, i)
                 swapped = True
+                if simulation == True:
+                    print arr
     return arr
