41dbd32d78b8eca5063e0728354d8a605821ef1a
Lee Geon <lg970325@naver.com>
2018-06-07 19:14:07 +0900

    Added simulation in bubble sort (#327)
    * Update bubble_sort.py
    
    * Update bubble_sort.py
    
    * Update bubble_sort.py
    
    * Update bubble_sort.py

--- algorithms/sort/bubble_sort.py
+++ algorithms/sort/bubble_sort.py
@@ -4,19 +4,29 @@ https://en.wikipedia.org/wiki/Bubble_sort
 
 Worst-case performance: O(N^2)
 
+If you call bubble_sort(arr,True), you can see the process of the sort
+Default is simulation = False
+
 """
 
 
-def bubble_sort(arr):
+def bubble_sort(arr, simulation=False):
     def swap(i, j):
         arr[i], arr[j] = arr[j], arr[i]
 
     n = len(arr)
     swapped = True
+    
+    if simulation:
+        print(arr)
+    
     while swapped:
         swapped = False
         for i in range(1, n):
             if arr[i - 1] > arr[i]:
                 swap(i - 1, i)
                 swapped = True
+                if simulation:
+                    print(arr)
+                    
     return arr
