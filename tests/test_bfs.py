b61a1da9b87ded3c3016ad8ae1d57739216edb68
Lee Geon <lg970325@naver.com>
2018-06-10 02:02:12 +0900

    Update test_bfs.py

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -1,10 +1,10 @@
-from algorithms.bfs import(
+from algorithms.bfs import (
     maze_search
 )
-    
+
 import unittest
 
-class TestSuite(unittest.TestCase):
+class TestMazeSearch(unittest.TestCase):
     
     def test_maze_search(self):
         grid = [[1,0,1,1,1,1],[1,0,1,0,1,0],[1,0,1,0,1,1],[1,1,1,0,1,1]]
