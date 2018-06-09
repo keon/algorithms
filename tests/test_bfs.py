ae473a46036a5ccdd28b3234c7df25db4fd54527
Lee Geon <lg970325@naver.com>
2018-06-10 02:26:29 +0900

    Update test_bfs.py

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -9,8 +9,10 @@ import unittest
 class TestMazeSearch(unittest.TestCase):
     
     def test_maze_search(self):
-        grid = [[1,0,1,1,1,1],[1,0,1,0,1,0],[1,0,1,0,1,1],[1,1,1,0,1,1]]
-        self.assertEqual(14, maze_search(grid))
+        grid_1 = [[1,0,1,1,1,1],[1,0,1,0,1,0],[1,0,1,0,1,1],[1,1,1,0,1,1]]
+        self.assertEqual(14, maze_search(grid_1))
+        grid_2 = [[1,0,0],[0,1,1],[0,1,1]]
+        self.assertEqual(-1, maze_search(grid_2))
         
 if __name__ == "__main__":
     unittest.main()
