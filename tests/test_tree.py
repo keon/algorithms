39813421f09a9779be4d88dd32a216e6b4ade413
Lee Geon <lg970325@naver.com>
2018-06-11 01:25:59 +0900

    Create test_tree.py

---
+++ tests/test_tree.py
@@ -0,0 +1,36 @@
+from algorithms.tree.traversal import (
+    preorder,
+    postorder
+)
+
+import unittest
+
+class TestTraversal(unittest.TestCase):
+    
+    def test_preorder(self):
+        n1 = Node(100)
+        n2 = Node(50)
+        n3 = Node(150)
+        n4 = Node(25)
+        n5 = Node(75)
+        n6 = Node(125)
+        n7 = Node(175)
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, n5
+        n3.left, n3.right = n6, n7
+        self.assertEqual([100, 50, 25, 75, 150, 125, 175], preorder(n1))
+        self.assertEqual([100, 50, 25, 75, 150, 125, 175], preorder_rec(n1))
+        
+    def test_postorder(self):
+        n1 = Node(100)
+        n2 = Node(50)
+        n3 = Node(150)
+        n4 = Node(25)
+        n5 = Node(75)
+        n6 = Node(125)
+        n7 = Node(175)
+        n1.left, n1.right = n2, n3
+        n2.left, n2.right = n4, n5
+        n3.left, n3.right = n6, n7
+        self.assertEqual([25, 75, 50, 125, 175, 150, 100], postorder(n1))
+        self.assertEqual([25, 75, 50, 125, 175, 150, 100], postorder_rec(n1))
