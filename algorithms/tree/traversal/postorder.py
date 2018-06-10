98c6975d3a2850e4ed19f6f08c22e5bbc745f734
Lee Geon <lg970325@naver.com>
2018-06-11 01:25:55 +0900

    Update postorder.py

--- algorithms/tree/traversal/postorder.py
+++ algorithms/tree/traversal/postorder.py
@@ -39,19 +39,3 @@ def postorder_rec(root, res=None):
     res.append(root.val)
     return res
 
-
-
-if __name__ == '__main__':
-    n1 = Node(100)
-    n2 = Node(50)
-    n3 = Node(150)
-    n4 = Node(25)
-    n5 = Node(75)
-    n6 = Node(125)
-    n7 = Node(175)
-    n1.left, n1.right = n2, n3
-    n2.left, n2.right = n4, n5
-    n3.left, n3.right = n6, n7
-
-    assert postorder(n1)     == [25, 75, 50, 125, 175, 150, 100]
-    assert postorder_rec(n1) == [25, 75, 50, 125, 175, 150, 100]
