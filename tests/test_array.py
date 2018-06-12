737adc8c513dd441f4642cb887659df3517f947d
aig031 <31883686+aig031@users.noreply.github.com>
2018-06-13 00:31:51 +0900

    Update test_array.py

--- tests/test_array.py
+++ tests/test_array.py
@@ -328,7 +328,7 @@ class TestLimit(unittest.TestCase):
     def test_limit(self):
         self.assertListEqual(limit([1, 2, 3, 4, 5], 2, 4), [2, 3, 4])
         self.assertListEqual(limit([1, 2, 3, 4, 5], 2), [2, 3, 4, 5])
-        self.assertListEqual(limit([1, 2, 3, 4, 5], 'unlimit', 4), [1, 2, 3, 4])
+        self.assertListEqual(limit([1, 2, 3, 4, 5], None, 4), [1, 2, 3, 4])
 
         
 
