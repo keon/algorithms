18b9ce123042bec808b7f2736fa8f4549afe2493
ppuzzle <wldn0202@naver.com>
2018-06-14 12:51:29 +0900

    added simulation to bogo_sort.py (#355)

--- algorithms/sort/bogo_sort.py
+++ algorithms/sort/bogo_sort.py
@@ -1,11 +1,16 @@
 import random
 
-def bogo_sort(arr):
+def bogo_sort(arr, simulation=False):
     """Bogo Sort
         Best Case Complexity: O(n)
         Worst Case Complexity: O(∞)
         Average Case Complexity: O(n(n-1)!)
     """
+    
+    iteration = 0
+    if simulation:
+        print("iteration",iteration,":",*arr)
+    
     def is_sorted(arr):
         #check the array is inorder
         i = 0
@@ -14,9 +19,14 @@ def bogo_sort(arr):
             if arr[i] > arr[i+1]:
                 return False
             i += 1
+            
+
         return True
     while not is_sorted(arr):
         random.shuffle(arr)
+        
+        if simulation:
+            iteration = iteration + 1
+            print("iteration",iteration,":",*arr)
+            
     return arr
-
-
