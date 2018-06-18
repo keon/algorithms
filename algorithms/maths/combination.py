e10a71ada80ad088c3325f8838acefd2afe173f3
Rahul Goswami <rahulg.cse16@nituk.ac.in>
2018-06-19 02:25:09 +0530

    update function combination_memo

--- algorithms/maths/combination.py
+++ algorithms/maths/combination.py
@@ -1,17 +1,17 @@
 def combination(n, r):
-    # This function calculates nCr
+    """This function calculates nCr."""
     if n == r or r == 0:
         return 1
     else:
         return combination(n-1, r-1) + combination(n-1, r)
-def combination_memo(n, r, c):
-    # This function calculates nCr, memoization method
-    if n == r or r == 0:
-        return 1
-    if None != c.get((n, r)):
-        return c[(n, r)]
-    elif None == c.get((n, r)):
-        c[(n-1, r-1)] = combination_memo(n-1, r-1, c)
-        c[(n-1, r)] = combination_memo(n-1, r, c)
-        c[(n, r)] = c[(n-1, r-1)] + c[(n-1, r)]
-        return c[(n, r)]    
+
+def combination_memo(n, r):
+    """This function calculates nCr using memoization method."""
+    memo = {}
+    def recur(n, r):
+        if n == r or r == 0:
+            return 1
+        if (n, r) not in memo:
+            memo[(n, r)] = recur(n - 1, r - 1) + recur(n - 1, r)
+        return memo[(n, r)]
+    return recur(n, r)
