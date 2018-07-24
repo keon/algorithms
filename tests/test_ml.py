a9787738f1b6808d7eb979a9f42d24d0e540802b
Christian Bender <christianbender89@web.de>
2018-07-24 23:26:42 +0200

    added empty lines

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -25,5 +25,7 @@ class TestML(unittest.TestCase):
         self.assertEqual(nearest_neighbor((230, 52, 239), self.trainSetLight), 'L')
     def test_distance(self):
         self.assertAlmostEqual(distance((1,2,3), (1,0,-1)), 4.47, 2)
+
+        
 if __name__ == "__main__":
     unittest.main()
