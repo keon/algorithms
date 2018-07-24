19c2596e1bf4a0be46256fefa653452235529d92
Christian Bender <christianbender89@web.de>
2018-07-24 23:17:33 +0200

    added a test. removed test cases. renamed directory

--- algorithms/ml/nearest_neighbor.py
+++ algorithms/ml/nearest_neighbor.py
@@ -1,13 +1,5 @@
 import numpy
 
-# train set for the AND-function
-trainSetAND = {(0,0) : 0, (0,1) :0, (1,0) : 0, (1,1) : 1} 
-
-# train set for light or dark colors
-trainSetLight = {(11, 98, 237) : 'L', (3, 39, 96) : 'D', (242, 226, 12) : 'L', (99, 93, 4) : 'D',
-(232, 62, 32) : 'L', (119, 28, 11) : 'D', (25, 214, 47) : 'L', (89, 136, 247) : 'L',
-(21, 34, 63) : 'D', (237, 99, 120) : 'L', (73, 33, 39) : 'D'}
-
 def distance(x,y):
     """[summary]
     HELPER-FUNCTION
@@ -47,11 +39,3 @@ def nearest_neighbor(x, tSet):
             min_d = d
             current_key = key
     return tSet[current_key]
-
-# Some test cases
-
-# print(nearest_neighbor((1,1), trainSetAND)) # => 1
-# print(nearest_neighbor((0,1), trainSetAND)) # => 0
-# print(nearest_neighbor((31, 242, 164), trainSetLight)) # => L
-# print(nearest_neighbor((13, 94, 64), trainSetLight)) # => D
-# print(nearest_neighbor((230, 52, 239), trainSetLight)) # => L
