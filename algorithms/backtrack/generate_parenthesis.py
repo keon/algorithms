342f95921a075f2e9a9d258e0ec1e65e71a33016
RenliangShi <creekee.idv@gmail.com>
2018-07-27 18:15:33 +0800

    add test and rename the function

--- algorithms/backtrack/generate_parenthesis.py
+++ algorithms/backtrack/generate_parenthesis.py
@@ -14,7 +14,7 @@ For example, given n = 3, a solution set is:
 """
 
 
-def generate_parenthesis(n):
+def  generate_parenthesis_v1(n):
     def add_pair(res, s, left, right):
         if left == 0 and right == 0:
             res.append(s)
@@ -29,13 +29,13 @@ def generate_parenthesis(n):
     return res
 
 
-def generate_parenthesis_(n):
+def generate_parenthesis_v2(n):
     def add_pair(res, s, left, right):
         if left == 0 and right == 0:
             res.append(s)
         if left > 0:
             add_pair(res, s+"(", left-1, right)
-        if right > 0 && left<right:
+        if right > 0 and left<right:
             add_pair(res, s+")", left, right-1)
 
     res = []
