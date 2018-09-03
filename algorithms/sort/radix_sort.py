00ef77c6fe8b61b85108afb6945c1c99a0745977
InnoFang <innofang@outlook.com>
2018-09-03 08:32:02 +0800

    fix sort/radix_sort (#395)

--- algorithms/sort/radix_sort.py
+++ algorithms/sort/radix_sort.py
@@ -1,24 +1,21 @@
 """
 radix sort
-complexity: O(nk) . n is the size of input list and k is the digit length of the number
+complexity: O(nk + n) . n is the size of input list and k is the digit length of the number
 """
 def radix_sort(arr, simulation=False):
-    is_done = False
     position = 1
+    max_number = max(arr)
 
     iteration = 0
     if simulation:
-        print("iteration",iteration,":",*arr)
+        print("iteration", iteration, ":", *arr)
 
-    while not is_done:
+    while position < max_number:
         queue_list = [list() for _ in range(10)]
-        is_done = True
 
         for num in arr:
             digit_number = num // position % 10
             queue_list[digit_number].append(num)
-            if is_done and digit_number > 0:
-                is_done = False
 
         index = 0
         for numbers in queue_list:
@@ -28,7 +25,7 @@ def radix_sort(arr, simulation=False):
 
         if simulation:
             iteration = iteration + 1
-            print("iteration",iteration,":",*arr)
+            print("iteration", iteration, ":", *arr)
 
         position *= 10
     return arr
