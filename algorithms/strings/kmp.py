81e4ec229fe75ce333964c843ec9710e466b0ba0
nolanding <deekshachandvani@gmail.com>
2018-10-01 15:00:05 +0530

    added kmp algorithm in srtings

---
+++ algorithms/strings/kmp.py
@@ -0,0 +1,58 @@
+# Python program for KMP Algorithm 
+# KMP algorithm is used to search if a particular string pattern is a substring in given txt string.
+def KMPSearch(pat, txt): 
+    M = len(pat) 
+    N = len(txt) 
+  
+    # create lps[] that will hold the longest prefix suffix  
+    # values for pattern 
+    lps = [0]*M 
+    j = 0 # index for pat[] 
+  
+    # Preprocess the pattern (calculate lps[] array) 
+    computeLPSArray(pat, M, lps) 
+  
+    i = 0 # index for txt[] 
+    while i < N: 
+        if pat[j] == txt[i]: 
+            i += 1
+            j += 1
+  
+        if j == M: 
+            print "Found pattern at index " + str(i-j) 
+            j = lps[j-1] 
+  
+        # mismatch after j matches 
+        elif i < N and pat[j] != txt[i]: 
+            # Do not match lps[0..lps[j-1]] characters, 
+            # they will match anyway 
+            if j != 0: 
+                j = lps[j-1] 
+            else: 
+                i += 1
+  
+def computeLPSArray(pat, M, lps): 
+    len = 0 
+  
+    lps[0] # lps[0] is always 0 
+    i = 1
+  
+   
+    while i < M: 
+        if pat[i]== pat[len]: 
+            len += 1
+            lps[i] = len
+            i += 1
+        else: 
+            if len != 0: 
+                len = lps[len-1] 
+  
+                # Also, note that we do not increment i here 
+            else: 
+                lps[i] = 0
+                i += 1
+  
+txt = "ALGORITHMALGOGONE"
+pat = "ALGOGONE"
+KMPSearch(pat, txt) 
+  
