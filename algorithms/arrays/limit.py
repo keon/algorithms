bac3222c1dd51741887a2187ef0be10e8902c5af
Rick Nickerson <feygon@gmail.com>
2018-10-07 20:18:42 -0700

    PEP8 style sweep (#393)
    Without affecting test ratios, I have made a sweep of the Arrays
    algorithms, and improved style, commentary, whitespace, and some
    translations
    
    flatten.py -- added commentary
    
    Garage.py -- added commentary, enforced line width on existing
    commentary
    
    Josephus.py -- enforced commentary alignment, binary operator spacing
    
    Limit.py -- enforced binary operator spacing, enforced commentary width
    This function is very similar to Python array-slicing, except by value.
    
    longest_non_repeat.py -- enforced spacing style in commentary
    added get functions
    Enforced function whitespace of one carriage return
    
    merge_intervals.py -- switched docstrings so the subject exposition was
    in the intro and the action done by the class was under the class,
    cleaned up other docstrings
    -- this class does need a docstring, but not the mathematician's
    exposition. What does this object do?
    
    n_sum.py -- added whitespace around recursive call, added (TL:DR)
    commentary, added docstrings, added commentary identifying recursive
    call, enforced line width and pep8 line break style WRT parameters on
    newlines w/ parentheses (e.g. lines 81-84, 88-90), enforced spelling
    consistency in argument parameters (e.g. target/taget)
    
    plus_one.py -- removed unused variable in plus_one_v3 method, clarified
    opening docstring
    
    rotate.py -- added comment line 24 for intentionally unused variable,
    enforced whitespace guidelines between methods
    
    three_sum.py -- specified opening docstring to indicate distinct
    elements in solution sets
    
    top_1.py -- improved translation in commentary
    
    trimmean.py -- improved translation in commentary, removed leftover
    commented instrumentation

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -1,22 +1,23 @@
 """
-Sometimes you need to limit array result to use. Such as you only need the value over
-10 or, you need value under than 100. By use this algorithms, you can limit your array
-to specific value
+Sometimes you need to limit array result to use. Such as you only need the 
+ value over 10 or, you need value under than 100. By use this algorithms, you
+ can limit your array to specific value
 
-If array, Min, Max value was given, it returns array that contains values of given array
-which was larger than Min, and lower than Max. You need to give 'unlimit' to use only Min
-or Max.
+If array, Min, Max value was given, it returns array that contains values of 
+ given array which was larger than Min, and lower than Max. You need to give
+ 'unlimit' to use only Min or Max.
 
 ex) limit([1,2,3,4,5], None, 3) = [1,2,3]
 
 Complexity = O(n)
 """
 
+# tl:dr -- array slicing by value
 def limit(arr, min_lim = None, max_lim = None):
     result = []
     if min_lim == None:
         for i in arr:
-            if i<= max_lim:
+            if i <= max_lim:
                 result.append(i)
     elif max_lim == None:
         for i in arr:
