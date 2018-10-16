8694de54ad1efdc0c1ee5823e46cdc1370c9f2ec
Anton Perez <2671616+krimvp@users.noreply.github.com>
2018-10-17 01:29:38 +0200

    Fix array plus one v3 (#432)
    * Fixes code for linkedlist/copy_random_pointer
    
      - Fixes the code (was previously commented) by uncommenting it and
      implementing the missing class that was referenced
      - Creates 2 test cases for both solutions in the code
    
    * Fix tests for arrays/plus_one
    
      - There was an issue while trying to access an index in a for loop
      within the array package (plus_one_v3() function)
        - idx was a tuple since it was being created as an enumerate(list)
        but it was being used as a simple index to access the list.
        - Changes the idx to access the element of the tuple that is the
        actual index (not the value)
    
    * Reformat linkedlist copy_random_pointer
    
      - Remove classes. Now there are 2 versions of the same function
      - Remove tests.
        - Move tests to the tests directory. Since the structure of this
        node is slightly different (adding an atribute) there is a helper
        inner function that will create the nodes in the structure for the 2
        tests that were added.
    
    * Unify name of functions in copy_random_pointer.py

--- algorithms/arrays/plus_one.py
+++ algorithms/arrays/plus_one.py
@@ -42,7 +42,7 @@ def plus_one_v2(digits):
 def plus_one_v3(num_arr):
 
     for idx in reversed(list(enumerate(num_arr))):
-        num_arr[idx] = (num_arr[idx] + 1) % 10
-        if num_arr[idx]:
+        num_arr[idx[0]] = (num_arr[idx[0]] + 1) % 10
+        if num_arr[idx[0]]:
             return num_arr
     return [1] + num_arr
--- algorithms/linkedlist/copy_random_pointer.py
+++ algorithms/linkedlist/copy_random_pointer.py
@@ -4,35 +4,45 @@ pointer which could point to any node in the list or null.
 
 Return a deep copy of the list.
 """
-# from collections import defaultdict
+from collections import defaultdict
 
-# TODO: This requires to be rewritten -- commenting it out for now
-# class Solution0:
-  # @param head, a RandomListNode
-  # @return a RandomListNode
-  # def copyRandomList(self, head):
-  #   dic = dict()
-  #   m = n = head
-  #   while m:
-  #       dic[m] = RandomListNode(m.label)
-  #       m = m.next
-  #   while n:
-  #       dic[n].next = dic.get(n.next)
-  #       dic[n].random = dic.get(n.random)
-  #       n = n.next
-  #   return dic.get(head)
-#
-#
-# class Solution1:  # O(n)
-#   @param head, a RandomListNode
-#   @return a RandomListNode
-  # def copyRandomList(self, head):
-  #   copy = defaultdict(lambda: RandomListNode(0))
-  #   copy[None] = None
-  #   node = head
-  #   while node:
-  #       copy[node].label = node.label
-  #       copy[node].next = copy[node.next]
-  #       copy[node].random = copy[node.random]
-  #       node = node.next
-  #   return copy[head]
+
+class RandomListNode(object):
+    def __init__(self, label):
+        self.label = label
+        self.next = None
+        self.random = None
+
+
+def copy_random_pointer_v1(head):
+    """
+    :type head: RandomListNode
+    :rtype: RandomListNode
+    """
+    dic = dict()
+    m = n = head
+    while m:
+        dic[m] = RandomListNode(m.label)
+        m = m.next
+    while n:
+        dic[n].next = dic.get(n.next)
+        dic[n].random = dic.get(n.random)
+        n = n.next
+    return dic.get(head)
+
+
+# O(n)
+def copy_random_pointer_v2(head):
+    """
+    :type head: RandomListNode
+    :rtype: RandomListNode
+    """
+    copy = defaultdict(lambda: RandomListNode(0))
+    copy[None] = None
+    node = head
+    while node:
+        copy[node].label = node.label
+        copy[node].next = copy[node.next]
+        copy[node].random = copy[node.random]
+        node = node.next
+    return copy[head]
