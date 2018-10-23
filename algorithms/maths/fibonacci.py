de6076d981bfde6b4ef510ad2d68b7d9a635e510
Harlan Williams <41646656+makeroflight@users.noreply.github.com>
2018-10-23 14:48:06 -0500

    Added change requests for fibonacci.py

--- algorithms/maths/fibonacci.py
+++ algorithms/maths/fibonacci.py
@@ -1,33 +1,19 @@
-import numpy as np
+""" Fibonacci sequence
+Generates a list consisting of fibonacci numbers from F(0)
+to F(n) for the input n
 
-def fibonacci(n):
-    """ fibonacci
-    Author-- makeroflight
-
-
-    Generates a list consisting of fibonacci numbers from F(0)
-    to F(n) for the input n, and then estimates phi from F(n) /
-    F(n-1) and compares the result to ( 1 + sqrt(5) ) / 2.
-
-    For example
-    fibonacci(5) generates the list [1, 1, 2, 3, 5, 8], and then
-    estimates phi as 8/5.
-
-    Arguments:
-    n -- The nth fibonacci number to generate to.
-    """
+Arguments:
+n -- The nth fibonacci number to generate to.
+simulation -- whether or not the script should return info
+about an estimation of phi
+"""
+def fibonacci(n, simulation=False):
     f_nums = [1,1]
     for x in range(1,n):
         f_nums.append(f_nums[x-1] + f_nums[x])
-
-    phi = 2 * np.cos(np.pi/5)
-    err = np.round(((f_nums[-1]/f_nums[-2] - phi)/phi * 100), decimals=2)
-
-    print(f_nums)
-    print("F({}) - F({}) = {}".format(n, n-1, f_nums[-1]/f_nums[-2]))
-    print("{}% error.".format(np.round(abs(err))))
-
-# uncomment these to get the script to prompt for input
-
-# number = input("Enter a number n to generate up to the nth fibonacci number: ")
-# fibonacci(int(number))
+    if (simulation):
+        phi = (1 + 5**0.5)/2
+        print(f_nums)
+        print("F({}) - F({}) = {}".format(n, n-1, f_nums[-1]/f_nums[-2]))
+        print("The true value of phi is {}".format(phi))
+    return f_nums
