a7a78725c6b9805e4cddbfef711c6735f03ec35e
Ernest <iattempt.net@gmail.com>
2019-02-13 14:23:45 +0800

    Rename and hide implementations.

--- algorithms/tree/is_balanced.py
+++ algorithms/tree/is_balanced.py
@@ -1,18 +1,21 @@
 def is_balanced(root):
+    return __is_balanced_recursive(root)
+
+def __is_balanced_recursive(root):
     """
     O(N) solution
     """
-    return -1 != get_depth(root)
+    return -1 != __get_depth(root)
 
 
-def get_depth(root):
+def __get_depth(root):
     """
     return 0 if unbalanced else depth + 1
     """
     if root is None:
         return 0
-    left  = get_depth(root.left)
-    right = get_depth(root.right)
+    left  = __get_depth(root.left)
+    right = __get_depth(root.right)
     if abs(left-right) > 1:
         return -1
     return 1 + max(left, right)
