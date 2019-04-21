72fe378a963d709ba2800c3088712ea72a3fc790
Ray Cao <youdymoo@umich.edu>
2019-04-20 20:51:41 -0400

    add_matrix_multiplication

---
+++ algorithms/matrix/multiply.py
@@ -0,0 +1,36 @@
+"""
+This algorithm takes two compatible two dimensional matrix
+and return their product
+"""
+
+
+def multiply(multiplicand: list, multiplier: list) -> list:
+    """
+    :type A: List[List[int]]
+    :type B: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    multiplicand_row, multiplicand_col = len(
+        multiplicand), len(multiplicand[0])
+    multiplier_row, multiplier_col = len(multiplier), len(multiplier[0])
+    if(multiplicand_col != multiplier_row):
+        raise Exception(
+            "Multiplicand matrix not compatible with Multiplier matrix.")
+    # create a result matrix
+    result = [[0] * multiplier_col for i in range(multiplicand_row)]
+    for i in range(multiplicand_row):
+        for j in range(multiplier_col):
+            for k in range(len(multiplier)):
+                result[i][j] += multiplicand[i][k] * multiplier[k][j]
+    return result
+
+
+def main():
+    a = [[1, 2, 3], [2, 1, 1]]
+    b = [[1], [2], [3]]
+    c = multiply(a, b)
+    print(c)
+
+
+if __name__ == "__main__":
+    main()
