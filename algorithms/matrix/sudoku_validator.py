9aa342809394fa5074fd7773fcac7b4715fdc217
Ray Cao <youdymoo@umich.edu>
2019-04-22 00:36:05 -0400

    fix test_matrix

--- algorithms/matrix/sudoku_validator.py
+++ algorithms/matrix/sudoku_validator.py
@@ -82,32 +82,3 @@ def valid_solution_set (board):
                 return False
 
     return True
-
-# test cases
-# To avoid congestion I'll leave testing all the functions to the reader. Just change the name of the function in the below test cases.
-import unittest
-class TestSuite(unittest.TestCase):
-    def test_valid(self):
-        self.assertTrue(valid_solution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
-                         [6, 7, 2, 1, 9, 5, 3, 4, 8],
-                         [1, 9, 8, 3, 4, 2, 5, 6, 7],
-                         [8, 5, 9, 7, 6, 1, 4, 2, 3],
-                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                         [9, 6, 1, 5, 3, 7, 2, 8, 4],
-                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                         [3, 4, 5, 2, 8, 6, 1, 7, 9]]))
-
-    def test_invalid(self):
-        self.assertFalse(valid_solution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
-                         [6, 7, 2, 1, 9, 0, 3, 4, 9],
-                         [1, 0, 0, 3, 4, 2, 5, 6, 0],
-                         [8, 5, 9, 7, 6, 1, 0, 2, 0],
-                         [4, 2, 6, 8, 5, 3, 7, 9, 1],
-                         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-                         [9, 0, 1, 5, 3, 7, 2, 1, 4],
-                         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-                         [3, 0, 0, 4, 8, 1, 1, 7, 9]]))
-
-if __name__ == "__main__":
-    unittest.main()
