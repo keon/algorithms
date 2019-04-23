349e288d288a94a6da541b33809da042ec684058
everfight <cbb903601682@163.com>
2019-04-23 17:14:33 +0800

    Update limit.py
    1. replace == with is 
    2. remove spaces around =

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -13,13 +13,13 @@ Complexity = O(n)
 """
 
 # tl:dr -- array slicing by value
-def limit(arr, min_lim = None, max_lim = None):
+def limit(arr, min_lim=None, max_lim=None):
     result = []
-    if min_lim == None and max_lim:
+    if min_lim is None and max_lim:
         for i in arr:
             if i <= max_lim:
                 result.append(i)
-    elif max_lim == None and min_lim:
+    elif max_lim is None and min_lim:
         for i in arr:
             if i >= min_lim:
                 result.append(i)
