72a0a76e5a83c09494143180c69e0a69170cf867
jchrys <45776091+jchrys@users.noreply.github.com>
2019-08-23 09:35:17 +0900

    move_zeros.py more intuitive representation
    pythonic way

--- algorithms/arrays/move_zeros.py
+++ algorithms/arrays/move_zeros.py
@@ -8,19 +8,10 @@ The time complexity of the below algorithm is O(n).
 """
 
 
+
 # False == 0 is True
 def move_zeros(array):
-    result = []
-    zeros = 0
-
-    for i in array:
-            if i == 0 and type(i) != bool:  # not using `not i` to avoid `False`, `[]`, etc.
-                zeros += 1
-            else:
-                result.append(i)
-    
-    result.extend([0] * zeros)
-    return result
-
+    result = [x for x in array if (x != 0 and type(x)!=bool)]
+    return result + [0]*(len(array)- len(result))
 
 print(move_zeros([False, 1, 0, 1, 2, 0, 1, 3, "a"]))
