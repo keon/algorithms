9ddcb6afdf9213a1c2ebd21a0af2fc89f4cdd56b
deli365 <freernr@gmail.com>
2019-09-22 18:47:09 +0300

    Init rle_compression file

---
+++ algorithms/compression/rle_compression.py
@@ -0,0 +1,7 @@
+"""
+Run-length encoding (RLE) is a simple compression algorithm 
+that gets a stream of data as the input and returns a
+sequence of counts of consecutive data values in a row.
+When decompressed the data will be fully recovered as RLE
+is a lossless data compression.
+"""
