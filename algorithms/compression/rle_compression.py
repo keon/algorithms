bdb79849fda7769a8027e4d46c6b81e31ecf352f
deli365 <freernr@gmail.com>
2019-09-22 18:49:40 +0300

    Added encoding rle method

--- algorithms/compression/rle_compression.py
+++ algorithms/compression/rle_compression.py
@@ -5,3 +5,32 @@ sequence of counts of consecutive data values in a row.
 When decompressed the data will be fully recovered as RLE
 is a lossless data compression.
 """
+
+def encode_rle(input):
+    """
+    Gets a stream of data and compresses it
+    under a Run-Length Encoding.
+    :param input: The data to be encoded.
+    :return: The encoded string.
+    """
+    if not input: return ''
+
+    encoded_str = ''
+    prev_ch = ''
+
+    for ch in input:
+        count = 1
+
+        # Check If the subsequent character does not match
+        if ch != prev_ch:
+            # Add the count and character
+            if prev_ch:
+                encoded_str += str(count) + prev_ch
+            # Reset the count and set the character
+            count = 1
+            prev_ch = ch
+        else:
+            # Otherwise increment the counter
+            count += 1
+    else:
+        return encoded_str + (str(count) + prev_ch)
