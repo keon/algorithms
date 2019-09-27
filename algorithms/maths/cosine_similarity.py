7987698279670cb0dd8db4cd04b93b58c11423cd
Hojun Yoon <48780831+hojun-y@users.noreply.github.com>
2019-09-27 14:42:21 +0900

    Fix some typo

--- algorithms/maths/cosine_similarity.py
+++ algorithms/maths/cosine_similarity.py
@@ -1,10 +1,9 @@
 import math
 
 
-
 def _l2_distance(vec):
     """
-    Calculate the L2 distance(Euclidean distance) from two given vectors.
+    Calculate l2 distance from two given vectors.
     """
     norm = 0.
     for e in vec:
@@ -15,14 +14,14 @@ def _l2_distance(vec):
 
 def cosine_similarity(a, b):
     if len(a) != len(b):
-        raise ValueError("The two vectors must be the same length. Got " + str(len(a)) +  " and " + str(len(b)))
+        raise ValueError("The two vectors must be the same length. Got shape " + str(len(a)) + " and " + str(len(b)))
     
     norm_a = _l2_distance(a)
     norm_b = _l2_distance(b)
 
     similarity = 0.
 
-    # Calculate the dot product of two normalized vectors
+    # Calculate the dot product of two vectors
     for ae, be in zip(a, b):
         similarity += ae * be
     
