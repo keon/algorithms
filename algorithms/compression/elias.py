dd476971a73e776835f7ec9fbe674c445659bcf2
nkarampi <nkarampi@gmail.com>
2019-10-18 00:01:23 +0300

    Added more info about the codings.

--- algorithms/compression/elias.py
+++ algorithms/compression/elias.py
@@ -1,3 +1,11 @@
+"""
+Elias γ code or Elias gamma code is a universal code encoding positive integers.
+It is used most commonly when coding integers whose upper-bound cannot be determined beforehand.
+Elias δ code or Elias delta code is a universal code encoding the positive integers,
+that includes Elias γ code when calculating.
+Both were developed by Peter Elias.
+
+"""
 from math import log,ceil
 
 log2 = lambda x: log(x,2)
@@ -11,8 +19,13 @@ def binary(x,l=1):
 def unary(x):
 	return (x-1)*'1'+'0'
 
-# Elias implementation for both gamma/delta
 def elias_generic(lencoding, x):
+	"""
+	The compressed data is calculated in two parts. 
+	The first part is the unary number of 1 + ⌊log2(x)⌋. 
+	The second part is the binary number of x - 2^(⌊log2(x)⌋).
+	For the final result we add these two parts.
+	"""
 	if x == 0: return '0'
 	
 	first_part = 1 + int(log2(x))
@@ -24,7 +37,13 @@ def elias_generic(lencoding, x):
 	return lencoding(first_part) + binary(a,k)
 
 def elias_gamma(x):
+	"""
+	For the first part we put the unary number of x.
+	"""
 	return elias_generic(unary, x)
 
 def elias_delta(x):
+	"""
+	For the first part we put the elias_g of the number.
+	"""
 	return elias_generic(elias_gamma,x)
