dc4c5bd1c1f3d6d39c7e751928715652290aabfc
Swati <kswati1398@gmail.com>
2019-10-21 14:48:50 +0530

    Added Recursive Fibonacci with memoization in fib.py

--- algorithms/dp/fib.py
+++ algorithms/dp/fib.py
@@ -21,11 +21,51 @@ def fib_recursive(n):
 
 # print(fib_recursive(35)) # => 9227465 (slow)
 
+def fib_recursive_memoization(n,memo):
+    """[summary]
+    This algorithm computes the n-th fibbonacci number
+    in approximate O(n) time and O(n) space. 
+    The algorithm use dynamic programming top down
+    approach with memoization to save results for 
+    avoiding recomputations.
+    For example : fib(5) = fib(4)+fib(3)
+                  fib(4) = fib(3)+fib(2)
+                  where fib(3) will be recursively broken down into fib(2) 
+                  and fib(1) twice.
+    hence, fib(3) has to be computed twice.
+    To avoid this, we save the results in a list called memo. We first
+    check if the value is already available in the list, if yes, we return that
+    value, else we perform the computation.
+    
+    
+    Arguments:
+        n {[int]} -- [description]
+    
+    Returns:
+        [int] -- [description]
+    """
+
+    # precondition
+    assert n >= 0, 'n must be a positive integer'
+    if(memo[n-1]!=-1):  # check if value is already computed and stored
+        return memo[n-1] 
+
+    else: # if not then compute, store and return it.
+        memo[n-1] = fib_recursive(n-1) + fib_recursive(n-2)
+        return memo[n-1]
+
+# initialise the memo list
+# memo =[-1]*99 #if we want to find 100th fibonacci number
+# memo[0]=0
+# memo[1]=1
+# print(fib_recursive_memoization(99,memo)) # => 354224848179261915075
+
 def fib_list(n):
     """[summary]
     This algorithm computes the n-th fibbonacci number
     very quick. approximate O(n)
-    The algorithm use dynamic programming.
+    The algorithm use dynamic programming with bottom up
+    approach.
     
     Arguments:
         n {[int]} -- [description]
