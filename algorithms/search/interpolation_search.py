c0b98d8f58032db8792f05adb0d25d26817c1689
Akshay Pal <Akshaypal2903@gmail.com>
2019-10-22 12:15:00 +0530

    update interpolation search

--- algorithms/search/interpolation_search.py
+++ algorithms/search/interpolation_search.py
@@ -1,17 +1,17 @@
 # interpolation search work for sorted list
 
-def interpolation_search(numbersList,value):
+def interpolation_search(numbers_List,value):
     low = 0
-    high = len(numbersList) - 1
-    while numbersList[low] <= value and numbersList[high] >= value:
-        mid = int(low + ((value - numbersList[low])*(high - low))/(numbersList[high] - numbersList[low]))
-        if numbersList[mid]<value:
+    high = len(numbers_List) - 1
+    while numbers_List[low] <= value and numbers_List[high] >= value:
+        mid = int(low + ((value - numbers_List[low])*(high - low))/(numbers_List[high] - numbers_List[low]))
+        if numbers_List[mid]<value:
             low = mid + 1
-        elif numbersList[mid]>value:
+        elif numbers_List[mid]>value:
             high = mid - 1
         else:
             return mid
-    if numbersList[low] == value:
+    if numbers_List[low] == value:
         return low
     return None
 
