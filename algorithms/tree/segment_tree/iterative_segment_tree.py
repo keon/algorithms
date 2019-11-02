a02edb9cd3d42ff51e022365817b711373fe5b17
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2019-11-02 12:58:58 -0500

    Add an additional example and moves examples to the top

--- algorithms/tree/segment_tree/iterative_segment_tree.py
+++ algorithms/tree/segment_tree/iterative_segment_tree.py
@@ -1,9 +1,25 @@
 """
-SegmentTree creates a segment tree with a given array and function,
+SegmentTree creates a segment tree with a given array and a "commutative" function,
 this non-recursive version uses less memory than the recursive version and include:
 1. range queries in log(N) time
 2. update an element in log(N) time
-the function takes 2 values and returns the same type value
+the function should be commutative and takes 2 values and returns the same type value
+
+Examples -
+mytree = SegmentTree([2, 4, 5, 3, 4],max)
+print(mytree.query(2, 4))
+mytree.update(3, 6)
+print(mytree.query(0, 3)) ...
+
+mytree = SegmentTree([4, 5, 2, 3, 4, 43, 3], lambda a, b: a + b)
+print(mytree.query(0, 6))
+mytree.update(2, -10)
+print(mytree.query(0, 6)) ...
+
+mytree = SegmentTree([(1, 2), (4, 6), (4, 5)], lambda a, b: (a[0] + b[0], a[1] + b[1]))
+print(mytree.query(0, 2))
+mytree.update(2, (-1, 2))
+print(mytree.query(0, 2)) ...
 """
 
 
@@ -35,18 +51,3 @@ class SegmentTree:
                 res = self.tree[r] if res is None else self.fn(res, self.tree[r])
             l, r = (l + 1) // 2, (r - 1) // 2
         return res
-
-
-'''
-Example -
-mytree = SegmentTree([2,4,5,3,4],max)
-print(mytree.query(2, 4))
-mytree.update(3, 6)
-print(mytree.query(0, 3)) ...
-
-mytree = SegmentTree([4,5,2,3,4,43,3], lambda a, b: a + b)
-print(mytree.query(0,6))
-mytree.update(2, -10)
-print(mytree.query(0, 6)) ...
-
-'''
