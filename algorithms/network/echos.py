9141c97d347e6009316c84e694ef24579d6cf8cd
kckotcherlakota <37008639+kckotcherlakota@users.noreply.github.com>
2019-11-10 07:18:28 +0530

    Add files via upload

---
+++ algorithms/network/echos.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = '127.0.0.1'  
+PORT = 65432        
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
