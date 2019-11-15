5bb0c92d49f10fa52401036dc28e378ecafdd2fc
Anirban Chetia <bloodraven166@gmail.com>
2019-11-15 19:51:00 +0530

    Include Floyd's cycle finding algorithm.

--- algorithms/linkedlist/is_cyclic.py
+++ algorithms/linkedlist/is_cyclic.py
@@ -25,3 +25,56 @@ def is_cyclic(head):
         if runner == walker:
             return True
     return False
+
+# Approach 2 : using Floyd's cycle finding algorithm
+# Node class:
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+    def strnode (self):
+        print(self.data)
+
+# LinkedList class:
+class LinkedList:
+    def __init__(self):
+        self.numnodes = 0
+        self.head = None
+
+    def insertLast(self, data):
+        newnode = Node(data)
+        newnode.next = None
+        if self.head == None:
+            self.head = newnode
+            return
+
+        lnode = self.head
+        while lnode.next != None :
+            lnode = lnode.next
+        lnode.next = newnode # new node is now the last node
+        self.numnodes += 1
+
+   # Cycle/Loop detection function: (returns boolean true/false)
+    def has_cycle(self):    
+        slow, fast = self.head ,self.head  
+        while fast != None:       
+            if fast.next != None:
+                 fast = fast.next.next
+            else:
+                 return False
+            slow = slow.next  
+            if slow == fast:
+                print("--slow",slow.data, "fast",fast.data) 
+                return True    
+        return False
+
+# Create a LinkedList:
+linkedList = LinkedList()
+linkedList.insertLast("12")
+linkedList.insertLast("5")
+linkedList.insertLast("13")
+
+# Create a loop for testing:
+linkedList.head.next.next.next = linkedList.head; 
+# Check for cycle/loop:
+print(linkedList.has_cycle())
