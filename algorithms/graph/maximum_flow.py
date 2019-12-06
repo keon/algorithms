0f0b4248445aac601a6b36b362abd58ea75a8e7f
sonho00 <sonho00@naver.com>
2019-12-07 00:43:56 +0900

    [ADD] Added Dinic algorithm.

--- algorithms/graph/maximum_flow.py
+++ algorithms/graph/maximum_flow.py
@@ -74,3 +74,53 @@ def edmonds_karp(capacity, source, sink):
             p = par[p]
     return ret
 
+def dinic_bfs(capacity, flow, level, source, sink):
+    # BFS function for Dinic algorithm.
+    # Check whether sink is reachable only using edges that is not full.
+
+    v = len(capacity)
+    q = queue.Queue()
+    q.put(source)
+    level[source] = 0
+    while q.qsize():
+        front = q.get()
+        for i in range(v):
+            if level[i] == -1 and flow[front][i] < capacity[front][i]:
+                level[i] = level[front] + 1
+                q.put(i)
+    return level[sink] != -1
+def dinic_dfs(capacity, flow, level, idx, sink, work, f = 1<<63):
+    # DFS function for Dinic algorithm.
+    # Finds new flow using edges that is not full.
+    if idx == sink:
+        return f
+    v = len(capacity)
+    for work[idx] in range(work[idx], v):
+        nxt = work[idx]
+        if level[nxt] == level[idx] + 1 and flow[idx][nxt] < capacity[idx][nxt]:
+            tmp = dinic_dfs(capacity, flow, level, nxt, sink, work, min(f, capacity[idx][nxt] - flow[idx][nxt])) 
+            if tmp > 0:
+                flow[idx][nxt] += tmp
+                flow[nxt][idx] -= tmp
+                return tmp
+    return 0
+def dinic(capacity, source, sink):
+    # Computes maximum flow from source to sink using Dinic algorithm.
+    # Time complexity : O(V^2*E)
+    # V is the number of vertices and E is the number of edges.
+    v = len(capacity)
+    flow = [[0]*v for i in range(v)]
+    ret = 0
+    while True:
+        level = [-1 for i in range(v)]
+        work = [0 for i in range(v)]
+        if not dinic_bfs(capacity, flow, level, source, sink):
+            break
+        while True:
+            tmp = dinic_dfs(capacity, flow, level, source, sink, work)
+            if tmp > 0:
+                ret += tmp
+            else:
+                break
+    return ret
+
