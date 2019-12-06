4cb21157280bd358f56d9dce85844e026ce4231a
sonho00 <sonho00@naver.com>
2019-12-06 14:09:32 +0900

    [EDIT] Added spaces to code to easier to read.

--- algorithms/maths/gcd.py
+++ algorithms/maths/gcd.py
@@ -14,7 +14,7 @@ def lcm(a, b):
 
 """
 Given a positive integer x, computes the number of trailing zero of x.
-Example)
+Example
 Input : 34(100010)
            ~~~~~^
 Output : 1
@@ -25,10 +25,10 @@ Output : 3
 """
 
 def trailing_zero(x):
-    cnt=0
-    while x and not x&1:
-        cnt+=1
-        x>>=1
+    cnt = 0
+    while x and not x & 1:
+        cnt += 1
+        x >>= 1
     return cnt
 
 """
@@ -36,15 +36,16 @@ Given two non-negative integer a and b,
 computes the greatest common divisor of a and b using bitwise operator.
 """
 
-def gcd_bit(a,b):
-    tza=trailing_zero(a)
-    tzb=trailing_zero(b)
-    a>>=tza
-    b>>=tzb
+def gcd_bit(a, b):
+    tza = trailing_zero(a)
+    tzb = trailing_zero(b)
+    a >>= tza
+    b >>= tzb
     while b:
-        if a<b: a,b=b,a
-        a-=b
-        a>>=trailing_zero(a)
-    return a<<min(tza,tzb)
+        if a < b: 
+            a, b = b, a
+        a -= b
+        a >>= trailing_zero(a)
+    return a << min(tza, tzb)
 
 
