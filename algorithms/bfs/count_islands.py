95fc3982318ffa1f16c39f745e3b405c3d1e8321
Hyunjin <guswh11@naver.com>
2019-12-09 15:44:31 +0900

    [Edit] Add examples in count_islands problem
    Add two examples in count_islands problem in bfs section.

--- algorithms/bfs/count_islands.py
+++ algorithms/bfs/count_islands.py
@@ -5,4 +5,18 @@ count the number of islands.
 An island is surrounded by water and is formed by
 connecting adjacent lands horizontally or vertically.
 You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+11110
+11010
+11000
+00000
+Answer: 1
+
+Example 2:
+11000
+11000
+00100
+00011
+Answer: 3
 """
