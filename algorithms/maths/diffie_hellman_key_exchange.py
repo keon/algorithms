5f92e1d107c32a48485efdcd144b7953201184b5
D-Sinus <peoplebj@naver.com>
2019-12-09 18:37:34 +0900

    [Add] Supplemental for DH Key Exchange

--- algorithms/maths/diffie_hellman_key_exchange.py
+++ algorithms/maths/diffie_hellman_key_exchange.py
@@ -95,28 +95,58 @@ def find_primitive_root(n):
         return p_root_list
 
 
+"""
+Diffie-Hellman key exchange is the method that enables
+two entities (in here, Alice and Bob), not knowing each other,
+to share common secret key through not-encrypted communication network.
+This method use the property of one-way function (discrete logarithm)
+For example, given a, b and n, it is easy to calculate x
+that satisfies (a^b) ≡ x (mod n).
+However, it is very hard to calculate x that satisfies (a^x) ≡ b (mod n).
+"""
+
 def alice_private_key(p):
+    """Alice determine her private key
+    in the range of 1 ~ p-1.
+    This must be kept in secret"""
     return randint(1, p-1)
+
 
 def alice_public_key(a_pr_k, a, p):
+    """Alice calculate her public key
+    with her private key.
+    This is open to public"""
     return pow(a, a_pr_k) % p
 
-def alice_shared_key(b_pu_k, a_pr_k, p):
-    return pow(b_pu_k, a_pr_k) % p
 
 def bob_private_key(p):
+    """Bob determine his private key
+    in the range of 1 ~ p-1.
+    This must be kept in secret"""
     return randint(1, p-1)
 
+
 def bob_public_key(b_pr_k, a, p):
-    return pow(a, b_pr_k) % p 
+    """Bob calculate his public key
+    with his private key.
+    This is open to public"""
+    return pow(a, b_pr_k) % p
+    
+
+def alice_shared_key(b_pu_k, a_pr_k, p):
+    """ Alice calculate secret key shared with Bob,
+    with her private key and Bob's public key.
+    This must be kept in secret"""
+    return pow(b_pu_k, a_pr_k) % p
+
 
 def bob_shared_key(a_pu_k, b_pr_k, p):
+    """ Bob calculate secret key shared with Alice,
+    with his private key and Alice's public key.
+    This must be kept in secret"""
     return pow(a_pu_k, b_pr_k) % p
 
 
-"""
-
-"""
 def diffie_hellman_key_exchange(a, p):
     if (prime_check(p) == False):
         print("%d is not a prime number" % p)
@@ -129,6 +159,7 @@ def diffie_hellman_key_exchange(a, p):
         except ValueError as e:
             print("%d is not a primitive root of %d" % (a, p))
             return -1
+            """ a must be primitive root of p """
         
         a_pr_k = alice_private_key(p)
         a_pu_k = alice_public_key(a_pr_k, a, p)
@@ -139,6 +170,9 @@ def diffie_hellman_key_exchange(a, p):
         b_pu_k = bob_public_key(b_pr_k, a, p)
         print ("Private key of Bob = %d" % b_pr_k)
         print ("Public key of Bob = %d" % b_pu_k)
+
+        """ In here, Alice send her public key to Bob,
+        and Bob also send his public key to Alice."""
 
         a_sh_k = alice_shared_key(b_pu_k, a_pr_k, p)
         b_sh_k = bob_shared_key(a_pu_k, b_pr_k, p)
