c9746da47caf3b9adfa7c56ba48e6626a7190be1
Hyunjin <guswh11@naver.com>
2019-12-09 15:45:57 +0900

    [Edit] Implement counting_islands method
    Complete counting_islands method.

--- algorithms/bfs/count_islands.py
+++ algorithms/bfs/count_islands.py
@@ -31,4 +31,22 @@ def counting_islands(grid):
     directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
     queue = []
 
+    for i in range(row):
+        for j, num in enumerate(grid[i]):
+            if num == 1 and visited[i][j] != 1:
+                visited[i][j] = 1
+                queue.append((i, j))
+                while len(queue) != 0:
+                    x = queue[0][0]
+                    y = queue[0][1]
+                    del queue[0]
+                    for k in range(len(directions)):
+                        nx_x = x + directions[k][0]
+                        nx_y = y + directions[k][1]
+                        if nx_x >= 0 and nx_y >= 0 and nx_x < row and nx_y < col:
+                            if visited[nx_x][nx_y] != 1 and grid[nx_x][nx_y] == 1:
+                                queue.append((nx_x, nx_y))
+                                visited[nx_x][nx_y] = 1
+                num_islands += 1
+
     return num_islands
