5e36bfd0eccdc922aa0bcad957b88ab90f52d499
NthreeAhn <39453590+NthreeAhn@users.noreply.github.com>
2019-12-09 18:08:52 +0900

    Update test_automata.py

--- tests/test_automata.py
+++ tests/test_automata.py
@@ -18,6 +18,19 @@ class TestDFA(unittest.TestCase):
         self.assertEqual(False, DFA(transitions, start, final, "000111100"))
         self.assertEqual(True, DFA(transitions, start, final, "111000011"))
         
+        transitions1 = {
+            '0': {'0': '1', '1': '0'},
+            '1': {'0': '2', '1': '0'},
+            '2': {'0': '2', '1': '3'},
+            '3': {'0': '3', '1': '3'}
+        }    
+        
+        final1 = ['0', '1', '2']
+        start1 = '0'
+        
+        self.assertEqual(False, DFA(transitions1, start1, final1, "0001111"))
+        self.assertEqual(True, DFA(transitions1, start1, final1, "01010101"))
+        
         transitions2 = {
             '0': {'a': '0', 'b': '1'},
             '1': {'a': '0', 'b': '2'},
