97caceb4d40e75304f3b97ba8712872b9840c243
Hyunjin <guswh11@naver.com>
2019-12-09 15:43:52 +0900

    [Edit] Add description in count_islands problem
    Add description in count_islands problem in bfs section.

---
+++ algorithms/bfs/count_islands.py
@@ -0,0 +1,8 @@
+"""
+This is a bfs-version of counting-islands problem in dfs section.
+Given a 2d grid map of '1's (land) and '0's (water),
+count the number of islands.
+An island is surrounded by water and is formed by
+connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+"""
