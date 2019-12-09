af6ced02605f36dad7e2ce11b13ef62200ed556c
Hyunjin <guswh11@naver.com>
2019-12-09 15:33:40 +0900

    [Edit] Add description and examples
    Add description and examples in maze_search problem.

---
+++ algorithms/dfs/maze_search.py
@@ -0,0 +1,20 @@
+'''
+Find shortest path from top left column to the right lowest column using DFS.
+only step on the columns whose value is 1
+if there is no path, it returns -1
+
+Ex 1)
+If maze is
+[[1,0,1,1,1,1],
+ [1,0,1,0,1,0],
+ [1,0,1,0,1,1],
+ [1,1,1,0,1,1]],
+the answer is: 14
+
+Ex 2)
+If maze is
+[[1,0,0],
+ [0,1,1],
+ [0,1,1]],
+the answer is: -1
+'''
