fc8ab6cd4ecdc33c1e17854b786ad41a6dbd6924
Estelle <1781043488@qq.com>
2019-12-17 16:38:52 +0800

    Update delete_nth.py
    use a dict solve this problem

--- algorithms/arrays/delete_nth.py
+++ algorithms/arrays/delete_nth.py
@@ -6,27 +6,14 @@ For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2],
 drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, 
 which leads to [1,2,3,1,2,3]
 """
-import collections
-
-
-# Time complexity O(n^2)
-def delete_nth_naive(array, n):
-    ans = []
-    for num in array:
-        if ans.count(num) < n:
-            ans.append(num)
-    return ans
-
-
-# Time Complexity O(n), using hash tables.
-def delete_nth(array, n):
-    result = []
-    counts = collections.defaultdict(int)  # keep track of occurrences
-
-    for i in array:
-
-        if counts[i] < n:
-            result.append(i)
-            counts[i] += 1
-
-    return result
+def maxrepeatN(A,N):
+    s = []
+    k = dict()
+    for i in A:
+        if i in k.keys():
+            k[i]+=1
+        else:
+            k[i] = 1
+        if k[i] <=N:
+            s.append(i)
+    return s
