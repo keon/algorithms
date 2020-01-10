8c787f50041ae93dd41d2df24d0bc66da35d2700
0verk1ll <22098433+0verk1ll@users.noreply.github.com>
2020-01-10 13:03:39 -0500

    Fix problems with testing equality to None.

--- algorithms/graph/tarjan.py
+++ algorithms/graph/tarjan.py
@@ -5,6 +5,7 @@ https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
 """
 from algorithms.graph.graph import DirectedGraph
 
+
 class Tarjan(object):
     def __init__(self, dict_graph):
         self.graph = DirectedGraph(dict_graph)
@@ -18,7 +19,7 @@ class Tarjan(object):
 
         self.sccs = []
         for v in self.graph.nodes:
-            if v.index == None:
+            if v.index is None:
                 self.strongconnect(v, self.sccs)
 
     def strongconnect(self, v, sccs):
@@ -31,7 +32,7 @@ class Tarjan(object):
 
         # Consider successors of v
         for w in self.graph.adjmt[v]:
-            if w.index == None:
+            if w.index is None:
                 # Successor w has not yet been visited; recurse on it
                 self.strongconnect(w, sccs)
                 v.lowlink = min(v.lowlink, w.lowlink)
@@ -41,7 +42,7 @@ class Tarjan(object):
                 # Note: The next line may look odd - but is correct.
                 # It says w.index not w.lowlink; that is deliberate and from the original paper
                 v.lowlink = min(v.lowlink, w.index)
-        
+
         # If v is a root node, pop the stack and generate an SCC
         if v.lowlink == v.index:
             # start a new strongly connected component
@@ -54,4 +55,3 @@ class Tarjan(object):
                     break
             scc.sort()
             sccs.append(scc)
-    
