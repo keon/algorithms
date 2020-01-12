716c78740a5a10b0456262a9ea249aec07957ffb
overkill <22098433+0verk1ll@users.noreply.github.com>
2020-01-12 05:57:45 +0000

    Removed Unused Var `carry` in multiply_srings.py (#628)
    * Fix problems with testing equality to None.
    
    * Removed unused variable

--- algorithms/automata/dfa.py
+++ algorithms/automata/dfa.py
@@ -3,16 +3,15 @@ def DFA(transitions, start, final, string):
     num = len(string)
     num_final = len(final)
     cur = start
-    
-    for i in range (num):
-        
-        if transitions[cur][string[i]] == None:
+
+    for i in range(num):
+
+        if transitions[cur][string[i]] is None:
             return False
         else:
             cur = transitions[cur][string[i]]
 
-    for i in range (num_final):
+    for i in range(num_final):
         if cur == final[i]:
             return True
     return False
-        
