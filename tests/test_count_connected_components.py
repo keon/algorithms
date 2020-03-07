25da9ad92062537d5f98e8e2aa6db1f16e7d8c56
Hardik dadhich <hadadhi1@in.ibm.com>
2020-03-07 14:05:57 +0530

    fixing spaces

--- tests/test_count_connected_components.py
+++ tests/test_count_connected_components.py
@@ -1,22 +1,25 @@
+#import modules
 import unittest
 from algorithms.graph import count_connected_number_of_component
 
 class TestConnectedComponentInGraph(unittest.TestCase):
-	""" Class """
+	""" Class to performs unit tests """
 	
 	def test_count_connected_components(self):
 		"""
 		   Test Function that test the different cases of count connected components
 	        
-	        0----------2    1--------5      3    
-            |
-            |
-            4
-            
-            output = 3
+		        0----------2    1--------5      3    
+	            |
+	            |
+	            4
+	            
+	            output = 3
 
 		"""
 		expected_result = 3
+		
+		# adjacency list representation of graph
 		l = [[2],
 		    [5],
 		    [0,4],
