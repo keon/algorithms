c8c6720eaef7321e7d90a9831aad56d5f08a7c7a
Uddeshya-1812 <uddeshya1812@gmail.com>
2020-08-24 17:01:27 +0530

    Updated for char arrays too

--- algorithms/arrays/flatten.py
+++ algorithms/arrays/flatten.py
@@ -3,29 +3,29 @@ Implement Flatten Arrays.
 Given an array that may contain nested arrays,
 produce a single resultant array.
 """
-from collections.abc import Iterable
+from collections.abc import  Iterable
 
+def flatten(arr,output = None):
+	if output is None:
+		output = []
+	
+	for ele in arr:
+		try: 	
+			if len(ele) > 0:
+				flatten(ele,output)		#tail recursion 
+		except:
+			output.append(ele)
+	return output
 
-# return list
-def flatten(input_arr, output_arr=None):
-    if output_arr is None:
-        output_arr = []
-    for ele in input_arr:
-        if isinstance(ele, Iterable):
-            flatten(ele, output_arr)    #tail-recursion
-        else:
-            output_arr.append(ele)      #produce the result
-    return output_arr
-
-
-# returns iterator
+#return iterator
 def flatten_iter(iterable):
-    """
-    Takes as input multi dimensional iterable and
-    returns generator which produces one dimensional output.
-    """
-    for element in iterable:
-        if isinstance(element, Iterable):
-            yield from flatten_iter(element)    
-        else:
-            yield element
+	"""
+	Takes as input multi dimensional iterable and
+	returns generator which produces one dimensional output.
+	"""
+	for element in iterable:
+		try:
+			if len(element) > 0:
+				yield from flatten_iter(element)	    	  
+		except:
+			yield element            
