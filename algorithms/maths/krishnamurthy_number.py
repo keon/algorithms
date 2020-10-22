a07b578bcf8796ffd870ca7322447040d92b6fcd
Nanak Bandyopadhyay <nanakbandyopadhay@hotmail.com>
2020-10-22 16:46:56 +0530

    tests added for krishnamurthy number, readme update

--- algorithms/maths/krishnamurthy_number.py
+++ algorithms/maths/krishnamurthy_number.py
@@ -24,23 +24,20 @@ def factorial(n):
     return fact
 
 
-def checkKrishnamurthy(n):
-    sumOfDigits = 0   # will hold sum of FACTORIAL of digits
+def krishnamurthy_number(n):
+    if n == 0:
+        return False
+    sum_of_digits = 0   # will hold sum of FACTORIAL of digits
     temp = n
 
     while temp != 0:
 
-        # get the factorial of of the last digit of n and add it to sumOfDigits
-        sumOfDigits += factorial(temp % 10)
+        # get the factorial of of the last digit of n and add it to sum_of_digits
+        sum_of_digits += factorial(temp % 10)
 
         # replace value of temp by temp/10
         # i.e. will remove the last digit from temp
         temp //= 10
 
     # returns True if number is krishnamurthy
-    return (sumOfDigits == n)
-
-
-if __name__ == '__main__':
-    n = 40585   # input number
-    print(checkKrishnamurthy(40585))
+    return (sum_of_digits == n)
