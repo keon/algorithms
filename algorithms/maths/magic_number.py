ac919c1f1cd4aab147edb17244ea1490fb5f7347
Nanak <36790357+Nanak360@users.noreply.github.com>
2020-10-29 21:19:25 +0530

    Updated variable names and condition statements

--- algorithms/maths/magic_number.py
+++ algorithms/maths/magic_number.py
@@ -15,20 +15,22 @@ The following function checks for Magic numbers and returns a Boolean accordingl
 
 
 def magic_number(n):
-    sum_total = 0
+    total_sum = 0
 
     # will end when n becomes 0
     # AND
     # sum becomes single digit.
-    while n > 0 or sum_total > 9:
-
-        if not n:
-            n = sum_total  # only when sum of digits isn't single digit
-            sum_total = 0
-        sum_total += n % 10
+    while n > 0 or total_sum > 9:
+        
+        # when n becomes 0 but we have a total_sum,
+        # we update the value of n with the value of the sum digits
+        if n == 0:
+            n = total_sum  # only when sum of digits isn't single digit
+            total_sum = 0
+        total_sum += n % 10
         n //= 10
 
     # Return true if sum becomes 1
-    return sum_total == 1
+    return total_sum == 1
 
 
