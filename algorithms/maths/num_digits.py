ea06c1afabe6bd0a5cb72d62199cf88fa7c7c6a1
Vignesh <ravivignesh2000@gmail.com>
2020-11-18 00:13:07 +0530

    Update num_digits.py

--- algorithms/maths/num_digits.py
+++ algorithms/maths/num_digits.py
@@ -8,9 +8,4 @@ def num_digits(n):
   if(n==0):
       return 1;
   return int(math.log10(n))+1
-
-if(__name__=='__main__'):
-  num=12345
-  print("Number of Digits in ",num," is : ",num_digits(num))
-  
   
