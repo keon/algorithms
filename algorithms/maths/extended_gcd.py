847dbdc750a165c98d28c7bf9bd1812d8c752247
Will Spichiger <wspichiger@gmail.com>
2021-02-08 23:53:00 -0500

    Corrected
    https://www.youtube.com/watch?v=f1wP28zeM3w&list=PL22w63XsKjqwAgBzVFVqZNMcVKpOOAA7c&index=6

--- algorithms/maths/extended_gcd.py
+++ algorithms/maths/extended_gcd.py
@@ -1,19 +1,15 @@
-def extended_gcd(a, b):
-    """Extended GCD algorithm.
-    Return s, t, g
-    such that a * s + b * t = GCD(a, b)
-    and s and t are co-prime.
-    """
+# Python program to demonstrate working of extended Euclidean Algorithm 
+	
+# function for extended Euclidean Algorithm 
+def gcdExtended(a, b): 
+	# Base Case 
+	if a == 0 : 
+		return b,0,1
+			
+	gcd,x1,y1 = gcdExtended(b%a, a) 
+	
+	# Update x and y using results of recursive call 
+	x = y1 - (b//a) * x1 
+	y = x1 
+	return gcd,x,y 
 
-    old_s, s = 1, 0
-    old_t, t = 0, 1
-    old_r, r = a, b
-    
-    while r != 0:
-        quotient = old_r / r
-        
-        old_r, r = r, old_r - quotient * r
-        old_s, s = s, old_s - quotient * s
-        old_t, t = t, old_t - quotient * t
-    
-    return old_s, old_t, old_r
