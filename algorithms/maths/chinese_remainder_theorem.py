afe8db1569c6295454253af531d7ee32efc057c1
Måns Anderssone <mans.andersson97@gmail.com>
2021-03-02 16:49:25 +0100

    feat: Add all n coprime check
    Co-authored-by: Lazar Cerovic<lazarc@kth.se>

--- algorithms/maths/chinese_remainder_theorem.py
+++ algorithms/maths/chinese_remainder_theorem.py
@@ -1,10 +1,14 @@
+from gcd import gcd
+
 def solve_chinese_remainder(num : list[int], rem : list[int]):
     """
     Computes the smallest x that satisfies the chinese remainder theorem
     """
-    k = len(num)
     if not len(num) == len(rem):
         raise Exception("num and rem should have equal length")
+    if not check_coprime(num):
+        raise Exception("All pairs of numbers in num are not coprime")
+    k = len(num)
     x = 1
     while True:
         i = 0
@@ -16,3 +20,12 @@ def solve_chinese_remainder(num : list[int], rem : list[int]):
             return x
         else:
             x += 1
+        
+def check_coprime(l : list[int]):
+    for i in range(len(l)):
+        for j in range(len(l)):
+            if i == j:
+                continue
+            if gcd(l[i], l[j]) != 1:
+                return False
+    return True
