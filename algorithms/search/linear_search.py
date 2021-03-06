b8168dd1c4860da3999d1bf68c2159459d9ff5c5
Felipe Veronezi Peters <fveronezipeters@gmail.com>
2021-03-05 23:42:46 -0300

    fix: Remove unsupported typing

--- algorithms/search/linear_search.py
+++ algorithms/search/linear_search.py
@@ -4,7 +4,7 @@ Linear search works in any array.
 
 
 # Time Complexity O(n) and Space Complexity O(n)
-def recursive_linear_search(array: list[int], index: int, target: int) -> int:
+def recursive_linear_search(array, index: int, target: int) -> int:
     if index < len(array):
         if array[index] == target:
             return index
