68be49ca496afe1f084bfc93e67247a27f0519fa
Renstrom <48918700+Renstrom@users.noreply.github.com>
2021-03-07 00:34:33 +0100

    Add 1-sparse-recovery streaming algorithm (#762)
    * feat:(first draft for the misra gries algorithm) #1
    
    * feat:(Added examples and changed to correct name) #1
    
    * feat:(Added init file for testing) #2
    
    * test:(Added tests for misras_gries function) #2
    
    * feat:(add 1-sparse recovery algorithm) #7
    
    * Add finalized 1-sparse-recovery algorithm
    
    * Renamed sparse function name to work with import
    
    * Tests added for 1-sparse-recovery function
    
    * Tests added for 1-sparse-recovery function
    
    Co-authored-by: callmeGoldenboy <natanteferi@gmail.com>

---
+++ tests/test_streaming.py
@@ -0,0 +1,15 @@
+from algorithms.streaming import (
+    one_sparse
+)
+import unittest   
+
+class TestOneSparse(unittest.TestCase):
+    def test_one_sparse_correct(self):
+        self.assertEqual(4,one_sparse([(4,'+'), (2,'+'),(2,'-'),(4,'+'),(3,'+'),(3,'-')]))
+        self.assertEqual(2,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+')]))
+
+
+    def test_one_sparse_incorrect(self):
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'+'),(1,'+')])) #Two values remaining
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(2,'+'),(2,'+'),(2,'-'),(2,'-'),(2,'-'),(2,'-')])) # No values remaining
+        self.assertEqual(None,one_sparse([(2,'+'),(2,'+'),(4,'+'),(4,'+')])) # Bitsum sum of sign is inccorect
