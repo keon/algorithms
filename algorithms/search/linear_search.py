71afaaaf4c28015612268843e0058e368b498b57
Felipe Veronezi Peters <fveronezipeters@gmail.com>
2021-03-05 23:21:06 -0300

    feat: Add recursive linear search

--- algorithms/search/linear_search.py
+++ algorithms/search/linear_search.py
@@ -1,8 +1,16 @@
-#
-# Linear search works in any array.
-#
-# T(n): O(n)
-#
+"""
+Linear search works in any array.
+"""
+
+
+# Time Complexity O(n) and Space Complexity O(n)
+def recursive_linear_search(array: list[int], index: int, target: int) -> int:
+    if index < len(array):
+        if array[index] == target:
+            return index
+        return recursive_linear_search(array, index + 1, target)
+    return -1
+
 
 def linear_search(array, query):
     for i in range(len(array)):
