81e9ca32e63aec5babb24a96799f68774e887294
Sahana <hola.sahana@gmail.com>
2021-05-21 15:20:25 +0530

    Adding binary to decimal

---
+++ algorithms/maths/binary_to_decimal.py
@@ -0,0 +1,9 @@
+number = list(input("Input a binary number: "))
+value = 0
+
+for i in range(len(number)):
+	digit = number.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print("The decimal value of the number is", value) 
+ ProTip! Use n and p to navigate between commits in a pull request.
