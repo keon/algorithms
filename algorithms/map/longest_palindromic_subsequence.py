9e6281419fcd4a2852a4ea51e4e50a5bbf04e185
kq58 <1772617802@qq.com>
2021-08-10 12:11:25 +0800

    Update longest_palindromic_subsequence.py

--- algorithms/map/longest_palindromic_subsequence.py
+++ algorithms/map/longest_palindromic_subsequence.py
@@ -1,22 +1,29 @@
-def longest_palindromic_subsequence(str):
-    n = len(str)
-  
-    # Create a table to store results of subproblems
-    L = [[0 for x in range(n)] for x in range(n)]
-  
-    for i in range(n):
-        L[i][i] = 1
-  
-    
-    for sub_string_length in range(2, n + 1):
-        for i in range(n-sub_string_length + 1):
-            j = i + sub_string_length-1
-            if str[i] == str[j] and sub_string_length == 2:
-                L[i][j] = 2
-            elif str[i] == str[j]:
-                L[i][j] = L[i + 1][j-1] + 2
-            else:
-                L[i][j] = max(L[i][j-1], L[i + 1][j]);
-  
-    return L[0][n-1]
+def longest_palindromic_subsequence(s):
 
+    k = len(s)
+    olist = [0] * k    # 申请长度为n的列表，并初始化
+    nList = [0] * k    # 同上
+    logestSubStr = ""
+    logestLen = 0
+
+    for j in range(0, k):
+        for i in range(0, j + 1):
+            if j - i <= 1:
+                if s[i] == s[j]:
+                    nList[i] = 1                 # 当 j 时，第 i 个子串为回文子串
+                    len_t = j - i + 1
+                    if logestLen < len_t:        # 判断长度
+                        logestSubStr = s[i:j + 1]
+                        logestLen = len_t
+            else:
+                if s[i] == s[j] and olist[i+1]:   # 当j-i>1时，判断s[i]是否等于s[j]，并判断当j-1时，第i+1个子串是否为回文子串
+                    nList[i] = 1                  # 当 j 时，第 i 个子串为回文子串
+                    len_t = j - i + 1
+                    if logestLen < len_t:
+                        logestSubStr = s[i:j + 1]
+                        logestLen = len_t
+        olist = nList                            # 覆盖旧的列表
+        nList = [0] * k                          # 新的列表清空
+    # ~ from icecream import ic
+    # ~ ic(s, logestSubStr)
+    return logestLen#, logestSubStr
