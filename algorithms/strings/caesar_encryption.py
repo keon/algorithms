7b156945e6c904c6650cd204f87a28a6b7e5ad1a
KarChunyip <karchunyip1118@gmail.com>
2021-08-24 20:59:06 +0800

    Add caesar encryption algorithm

---
+++ algorithms/strings/caesar_encryption.py
@@ -0,0 +1,26 @@
+"""
+while using caesar encryption the strings can be encryted based on the number of the steps
+exp: text = "abc" step = 2
+output: "cde"
+"""
+def caesar_encrypt(realText, step):
+    outText = []
+    cryptText = []
+    
+    uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    for eachLetter in realText:
+        if eachLetter in uppercase:
+            index = uppercase.index(eachLetter)
+            crypting = (index + step) % 26
+            cryptText.append(crypting)
+            newLetter = uppercase[crypting]
+            outText.append(newLetter)
+        elif eachLetter in lowercase:
+            index = lowercase.index(eachLetter)
+            crypting = (index + step) % 26
+            cryptText.append(crypting)
+            newLetter = lowercase[crypting]
+            outText.append(newLetter)
+    return outText
