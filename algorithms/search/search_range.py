f73fbb3f0547a7bedb8505afdaf11e16ac408b11
KishoreT <90593312+KishoreTG@users.noreply.github.com>
2021-09-13 15:40:31 +0530

    Fixed search_range function by implementing via binary search

--- algorithms/search/search_range.py
+++ algorithms/search/search_range.py
@@ -1,3 +1,64 @@
+
+import math
+
+def binary_search_leftmost(nums, target):
+
+    low = 0
+    high = len(nums) - 1
+
+    while low != high:
+
+        mid = math.floor((low + high) / 2)
+
+        # Element not found
+        if low > high:
+            return -1
+        
+        # Element found, but... check whether there exists a leftmost element
+        if target == nums[mid]:
+            high = mid
+
+        # Shrinken the array
+        if target < nums[mid]:
+            high = mid - 1
+        elif target > nums[mid]:
+            low = mid + 1
+    
+    # Final check whether the element is correct
+    if nums[low] == target:
+        return low
+    else:
+        return -1
+
+def binary_search_rightmost(nums, target):
+
+    low = 0
+    high = len(nums) - 1
+
+    while low != high:
+
+        mid = math.ceil((low + high) / 2)
+
+        # Element not found
+        if low > high:
+            return -1
+        
+        # Element found, but... check whether there exists a rightmost element
+        if target == nums[mid]:
+            low = mid
+
+        # Shrinken the array
+        if target < nums[mid]:
+            high = mid - 1
+        elif target > nums[mid]:
+            low = mid + 1
+    
+    # Final check whether the element is correct
+    if nums[low] == target:
+        return low
+    else:
+        return -1
+
 """
 Given an array of integers nums sorted in ascending order, find the starting
 and ending position of a given target value. If the target is not found in the
@@ -15,19 +76,15 @@ def search_range(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    low = 0
-    high = len(nums) - 1
-    while low <= high:
-        mid = low + (high - low) // 2
-        if target < nums[mid]:
-            high = mid - 1
-        elif target > nums[mid]:
-            low = mid + 1
-        else:
-            break
 
-    for j in range(len(nums) - 1, -1, -1):
-        if nums[j] == target:
-            return [mid, j]
+    leftSearch = binary_search_leftmost(nums, target)
+    rightSearch = binary_search_rightmost(nums, target)
 
-    return [-1, -1]
+    # Element is there
+    if leftSearch != -1:
+        result = [leftSearch, rightSearch]
+        return result
+
+    # Element is not there
+    else:
+        return [-1, -1]
