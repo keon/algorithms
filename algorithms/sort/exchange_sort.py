539471aeca247601075bd07e229803112724a45c
Kim-AYoung <onionlily123@naver.com>
2021-12-03 13:42:48 +0900

    move comment position

--- algorithms/sort/exchange_sort.py
+++ algorithms/sort/exchange_sort.py
@@ -1,13 +1,8 @@
-"""
-
-Reference : https://en.wikipedia.org/wiki/Sorting_algorithm#Exchange_sort
-
-Complexity : O(n^2)
-
-"""
-
 def exchange_sort(arr):
-    
+    """
+    Reference : https://en.wikipedia.org/wiki/Sorting_algorithm#Exchange_sort
+    Complexity : O(n^2)
+    """
     arr_len = len(arr)
     for i in range(arr_len-1):
         for j in range(i+1, arr_len):
