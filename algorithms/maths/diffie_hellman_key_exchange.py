f6976a2a423f9ead636462ab39ea2ca5b49b3bf5
Sammygarch <95525337+Sammygarch@users.noreply.github.com>
2022-02-01 14:51:02 +0000

    fixed spelling errors

--- algorithms/maths/diffie_hellman_key_exchange.py
+++ algorithms/maths/diffie_hellman_key_exchange.py
@@ -1,5 +1,5 @@
 import math
-from random import randint
+from random import _randint
 
 """
 Code from /algorithms/maths/prime_check.py,
@@ -27,13 +27,13 @@ def prime_check(n):
 """
 For positive integer n and given integer a that satisfies gcd(a, n) = 1,
 the order of a modulo n is the smallest positive integer k that satisfies
-pow (a, k) % n = 1. In other words, (a^k) ≡ 1 (mod n).
+pow(a, k) % n = 1. In other words, (a^k) ≡ 1 (mod n).
 Order of certain number may or may not be exist. If so, return -1.
 """
 def find_order(a, n):
     if ((a == 1) & (n == 1)):
         return 1
-        """ Exception Handeling :
+        """ Exception Handling :
         1 is the order of of 1 """
     else:
         if (math.gcd(a, n) != 1):
@@ -75,7 +75,7 @@ If so, return empty list.
 def find_primitive_root(n):
     if (n == 1):
         return [0]
-        """ Exception Handeling :
+        """ Exception Handling :
         0 is the only primitive root of 1 """
     else:
         phi = euler_totient(n)
