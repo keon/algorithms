bbad9f30c41e50ce5b0408664721f98e8bbfcceb
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-02-15 23:28:26 -0600

    Bug fix: Add None checks for min/max boundaries.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -14,4 +14,12 @@ Complexity = O(n)
 
 # tl:dr -- array slicing by value
 def limit(arr, min_lim=None, max_lim=None):
+    if len(arr) == 0:
+        return arr
+
+    if min_lim is None:
+        min_lim = min(arr)
+    if max_lim is None:
+        max_lim = max(arr)
+
     return list(filter(lambda x: (min_lim <= x <= max_lim), arr))
