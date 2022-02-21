8ce4c0b14ba2c79b0da5765be7e0878ba0de8f33
ArnPellesGit <pelle.arn@gmail.com>
2022-02-21 12:42:15 +0100

    Added testing for the fizzbuzz algorithm.

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -41,7 +41,8 @@ from algorithms.strings import (
     atbash,
     longest_palindrome,
     knuth_morris_pratt,
-    panagram
+    panagram,
+    fizzbuzz
 )
 
 import unittest
@@ -662,6 +663,33 @@ class TestPanagram(unittest.TestCase):
         # Assert
         self.assertEqual(True, res)
 
+class TestFizzbuzz(unittest.TestCase):
+    """[summary]
+    Tests for the fizzbuzz method in file fizzbuzz.py
+    """
+    
+    def test_fizzbuzz(self):
+       # Testing that n < 0 returns a Value Error
+       self.assertRaises(ValueError, fizzbuzz.fizzbuzz, -2)
 
+       # Testing that a string returns a Type Error.
+       self.assertRaises(TypeError, fizzbuzz.fizzbuzz, "hello")
+
+       # Testing a base case, n = 3
+       result = fizzbuzz.fizzbuzz(3)
+       expected = [1, 2, "Fizz"]
+       self.assertEqual(result, expected)
+
+       # Testing a base case, n = 5
+       result = fizzbuzz.fizzbuzz(5)
+       expected = [1, 2, "Fizz", 4, "Buzz"]
+       self.assertEqual(result, expected)
+
+       # Testing a base case, n = 15 i.e. mod 3 and 5
+       result = fizzbuzz.fizzbuzz(15)
+       expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, 
+       "Fizz", 13, 14, "FizzBuzz"]
+       self.assertEqual(result, expected)
+        
 if __name__ == "__main__":
     unittest.main()
