87f7cdfc1b299dcc2b01d54ef935db8fbe146e34
ersode <ersode@kth.se>
2022-03-07 02:46:23 +0100

    refactor #120: Moving moving_average tests to test_queues.py

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -4,8 +4,9 @@ from algorithms.queues import (
     ArrayQueue, LinkedListQueue,
     max_sliding_window,
     reconstruct_queue,
-    PriorityQueue
+    PriorityQueue,
 )
+from queues.moving_average import MovingAverage
 
 
 class TestQueue(unittest.TestCase):
@@ -158,6 +159,14 @@ class TestPriorityQueue(unittest.TestCase):
         queue.push(2)
         self.assertEqual(4, queue.size())
         self.assertEqual(2, queue.pop())
+
+class TestMovingAverage(unittest.TestCase):
+    def test_MovingAverage(self):
+        m = MovingAverage(3)
+        self.assertEqual(m.next(1), 1)
+        self.assertEqual(m.next(10), (1 + 10) / 2)
+        self.assertEqual(m.next(3), (1 + 10 + 3) / 3)
+        self.assertEqual(m.next(5), (10 + 3 + 5) / 3)
 
 
 if __name__ == "__main__":
