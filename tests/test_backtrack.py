02fca486b7a170b2a1442d93f7b1d12ece8f50d6
Fredrik Svanholm <fredrik.svanholm@gmail.com>
2022-03-07 11:26:24 +0100

    fixed import issue

--- tests/test_backtrack.py
+++ tests/test_backtrack.py
@@ -13,6 +13,7 @@ from algorithms.backtrack import (
     generate_parenthesis_v1,
     generate_parenthesis_v2,
     letter_combinations,
+    binary_minimax,
     minimax,
     palindromic_substrings,
     pattern_match,
@@ -253,8 +254,8 @@ class TestMiniMax(unittest.TestCase):
         n1.left, n1.right = n2, n3
         n2.left, n2.right = n4, n5
         n3.left, n3.right = n6, n7
-        a=binary_minimax(n1,2,True,-10,10)
-        self.assertEqual(binary_minimax(n1,2,True,-10,10),8)
+        a = binary_minimax(n1, 2, True, -10, 10)
+        self.assertEqual(binary_minimax(n1, 2, True, -10, 10), 8)
         
     def test_minmaxBinary2(self):
         n1 = Node(0)
