aed02b9ab73241fa97463f484abb9bface5cc07d
izveigor <izveigor@mail.ru>
2022-07-28 19:49:44 +0300

    Added randomized-quicksort

--- algorithms/sort/quick_sort.py
+++ algorithms/sort/quick_sort.py
@@ -1,26 +1,49 @@
-def quick_sort(arr, simulation=False):
+import random
+
+
+def quick_sort(arr, simulation=False, randomized=False):
     """ Quick sort
         Complexity: best O(n log(n)) avg O(n log(n)), worst O(N^2)
     """
-    
+
     iteration = 0
     if simulation:
-        print("iteration",iteration,":",*arr)
-    arr, _ = quick_sort_recur(arr, 0, len(arr) - 1, iteration, simulation)
+        print("iteration", iteration, ":", *arr)
+    arr, _ = quick_sort_recur(
+        arr, 0, len(arr) - 1,
+        iteration, simulation, randomized,
+    )
     return arr
 
-def quick_sort_recur(arr, first, last, iteration, simulation):
+
+def quick_sort_recur(arr, first, last, iteration, simulation, randomized):
     if first < last:
-        pos = partition(arr, first, last)
+        if randomized:
+            pos = randomized_partition(arr, first, last)
+        else:
+            pos = partition(arr, first, last)
         # Start our two recursive calls
         if simulation:
             iteration = iteration + 1
-            print("iteration",iteration,":",*arr)
-            
-        _, iteration = quick_sort_recur(arr, first, pos - 1, iteration, simulation)
-        _, iteration = quick_sort_recur(arr, pos + 1, last, iteration, simulation)
+            print("iteration", iteration, ":", *arr)
+
+        _, iteration = quick_sort_recur(
+            arr, first, pos - 1,
+            iteration, simulation, randomized,
+        )
+        _, iteration = quick_sort_recur(
+            arr, pos + 1, last,
+            iteration, simulation, randomized,
+        )
 
     return arr, iteration
+
+
+def randomized_partition(arr, first, last):
+    index = random.randint(first, last)
+    arr[last], arr[index] = arr[index], arr[last]
+    return partition(arr, first, last)
+
 
 def partition(arr, first, last):
     wall = first
@@ -30,3 +53,7 @@ def partition(arr, first, last):
             wall += 1
     arr[wall], arr[last] = arr[last], arr[wall]
     return wall
+
+
+if __name__ == "__main__":
+    quick_sort([1, 3, 4, 2, 5, 6, 3, 1], simulation=True, randomized=True)
