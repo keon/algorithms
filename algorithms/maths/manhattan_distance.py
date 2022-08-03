4129500e5926cd7bad0950cee3a9ddfc60e77220
ChinZhengSheng <dfrockstar2016@gmail.com>
2022-08-03 19:01:38 +0800

    Added Manhattan Distance

--- algorithms/maths/manhattan_distance.py
+++ algorithms/maths/manhattan_distance.py
@@ -8,10 +8,11 @@ The sum of absolute difference between coordinates of two points.
 distance = |x2 - x1| + |y2 - y1|
 
 """
-def manhattan_distance(coords1, coords2):
+def manhattan_distance(x,y):
     
     distance = 0
-    for x1, x2 in zip(coords1, coords2):
-            distance += abs(x1-x2)
+
+    for x1,x2 in zip(x,y):
+        distance += abs(x1-x2)
 
     return distance
