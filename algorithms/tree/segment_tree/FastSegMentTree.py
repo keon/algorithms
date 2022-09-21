9d95930dfb52cb112300b721e690b4b701e2e3fd
sdt4936 <sdt4936@gmail.com>
2022-09-21 06:19:29 +0530

    Updating

--- algorithms/tree/segment_tree/FastSegMentTree.py
+++ algorithms/tree/segment_tree/FastSegMentTree.py
@@ -1,8 +1,13 @@
 class SEG:
-    def __init__(self, n, fn):
-        self.n = n
-        self.fn = fn
-        self.tree = [0] * 2 * self.n
+    def __init__(self, arr, function):
+        self.tree = [None for _ in range(len(arr))] + arr
+        self.size = len(arr)
+        self.fn = function
+        self.build_tree()
+
+    def build_tree(self):
+        for i in range(self.size - 1, 0, -1):
+            self.tree[i] = self.fn(self.tree[i * 2], self.tree[i * 2 + 1])
        
     def query(self, l, r):
         l += self.n
