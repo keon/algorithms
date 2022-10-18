e41d65a88f09a8a011e5909c06e0aba6c63d359b
sdt4936 <sdt4936@gmail.com>
2022-10-18 14:12:51 +0530

    prime-factors-my-style

---
+++ algorithms/maths/find_prime_factors.py
@@ -0,0 +1,20 @@
+# Gives the shortest prime factor for a number
+import math
+N = 1000001
+spf = [-1] * N
+def shortesPrimeFactorForEachNum():
+    global spf
+    for i in range(4, N, 1):
+        spf[i] = 2
+    for i in range(3, math.ceil(N ** 0.5)):
+        if spf[i] == -1:
+            for j in range(i ** 2, N, i):
+                if spf[j] == -1:
+                    spf[j] = i
+shortesPrimeFactorForEachNum()
+def getPrimeFactorization(x):
+    ret = list()
+    while (x != 1):
+        ret.append(spf[x])
+        x = x // spf[x]
+    return ret
