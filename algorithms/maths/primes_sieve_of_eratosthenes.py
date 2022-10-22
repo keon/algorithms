a6a3c1e4958239242b970979962b9dc147f50477
sdt4936 <sdt4936@gmail.com>
2022-10-22 15:17:08 +0530

    bitwise

--- algorithms/maths/primes_sieve_of_eratosthenes.py
+++ algorithms/maths/primes_sieve_of_eratosthenes.py
@@ -31,14 +31,13 @@ def get_primes(n):
     if n <= 0:
         raise ValueError("'n' must be a positive integer.")
     # If x is even, exclude x from list (-1):
-    sieve_size = (n // 2 - 1) if n &1^1 else (n // 2)
+    sieve_size = (n >>1 - 1) if n &1^1 else (n >>1)
     sieve = [True for _ in range(sieve_size)]   # Sieve
     primes = []      # List of Primes
-    if n >= 2:
-        primes.append(2)      # 2 is prime by default
+    if n >= 2: primes.append(2)      # 2 is prime by default
     for i in range(sieve_size):
         if sieve[i]:
-            value_at_i = i*2 + 3
+            value_at_i = i << 1 + 3
             primes.append(value_at_i)
             for j in range(i, sieve_size, value_at_i):
                 sieve[j] = False
