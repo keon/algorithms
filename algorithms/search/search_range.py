e24247fb1455bc2d1ddfcd5d8923f9a35cd2a27a
Mohan <lmohan717@gmail.com>
2023-03-08 15:02:38 +0530

    Fix search_range and add test (#868)

--- algorithms/search/search_range.py
+++ algorithms/search/search_range.py
@@ -17,17 +17,17 @@ def search_range(nums, target):
     """
     low = 0
     high = len(nums) - 1
-    while low <= high:
+    # breaks at low == high
+    # both pointing to first occurence of target
+    while low < high:
         mid = low + (high - low) // 2
-        if target < nums[mid]:
-            high = mid - 1
-        elif target > nums[mid]:
-            low = mid + 1
+        if target <= nums[mid]:
+            high = mid
         else:
-            break
+            low = mid + 1
 
     for j in range(len(nums) - 1, -1, -1):
         if nums[j] == target:
-            return [mid, j]
+            return [low, j]
 
     return [-1, -1]
