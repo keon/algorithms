09a713c9da23b9830d002e90cb7cb8c24661a345
sdt4936 <sdt4936@gmail.com>
2023-11-03 19:22:25 +0530

    formatting

--- algorithms/shivsQuickBucket/binary_heap.py
+++ algorithms/shivsQuickBucket/binary_heap.py
@@ -1,3 +1,56 @@
+class BinaryHeap():
+    def __init__(self):
+        self.current_size = 0
+        self.heap = [(0)]
+
+    def up(self, i):
+        while i // 2 > 0:
+            if self.heap[i] < self.heap[i // 2]:
+                # Swap value of child with value of its parent
+                self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i]
+            i = i // 2
+
+    def insert(self, val):
+        self.heap.append(val)
+        self.current_size = self.current_size + 1
+        self.up(self.current_size)
+
+    def min_child(self, i):
+        if 2 * i + 1 > self.current_size:
+            # No right child
+            return 2 * i
+        if self.heap[2 * i] > self.heap[2 * i + 1]:
+            return 2 * i + 1
+        return 2 * i
+
+    def down(self, i):
+        while 2 * i < self.current_size:
+            min_child = self.min_child(i)
+            if self.heap[min_child] < self.heap[i]:
+                # Swap min child with parent
+                self.heap[min_child], self.heap[i] = self.heap[i], self.heap[min_child]
+            i = min_child
+
+    def remove_min(self):
+        popped = self.heap[1]
+        # the smallest value at beginning
+        # Replace it by the last value
+        self.heap[1] = self.heap[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap.pop()
+        self.down(1)
+        return popped
+
+
+
+
+
+
+
+
+
+
+
 """
 Binary Heap. A min heap is a complete binary tree where each node is smaller than
 its children. The root, therefore, is the minimum element in the tree. The min
@@ -29,61 +82,21 @@ For example remove_min() in a min heap:
 55 90 87             55  90                 55  90
 
 """
-class BinaryHeap():
-    def __init__(self):
-        self.current_size = 0
-        self.heap = [(0)]
-
-    def up(self, i):
-        while i // 2 > 0:
-            if self.heap[i] < self.heap[i // 2]:
-                # Swap value of child with value of its parent
-                self.heap[i], self.heap[i//2] = self.heap[i//2], self.heap[i]
-            i = i // 2
-
-    """
+"""
         Method insert always start by inserting the element at the bottom.
         It inserts rightmost spot so as to maintain the complete tree property.
         Then, it fixes the tree by swapping the new element with its parent,
         until it finds an appropriate spot for the element. It essentially
         ups the minimum element
         Complexity: O(logN)
-    """
-    def insert(self, val):
-        self.heap.append(val)
-        self.current_size = self.current_size + 1
-        self.up(self.current_size)
-
-    """
-    Method min_child returns the index of smaller of 2 children of parent at index i
-    """
-    def min_child(self, i):
-        if 2 * i + 1 > self.current_size:  # No right child
-            return 2 * i
-        if self.heap[2 * i] > self.heap[2 * i + 1]:
-            return 2 * i + 1
-        return 2 * i
-
-    def down(self, i):
-        while 2 * i < self.current_size:
-            min_child = self.min_child(i)
-            if self.heap[min_child] < self.heap[i]:
-                # Swap min child with parent
-                self.heap[min_child], self.heap[i] = self.heap[i], self.heap[min_child]
-            i = min_child
-
-    """
+"""
+"""
         Remove Min method removes the minimum element and swap it with the last
         element in the heap( the bottommost, rightmost element). Then, it
         downs this element, swapping it with one of its children until the
         min heap property is restored
         Complexity: O(logN)
-    """
-    def remove_min(self):
-        popped = self.heap[1]      # the smallest value at beginning
-        # Replace it by the last value
-        self.heap[1] = self.heap[self.current_size]
-        self.current_size = self.current_size - 1
-        self.heap.pop()
-        self.down(1)
-        return popped
+"""
+"""
+    Method min_child returns the index of smaller of 2 children of parent at index i
+"""
