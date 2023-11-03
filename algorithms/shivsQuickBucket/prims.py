1f55b194496db3a3079d6691b8f288ee3a064274
sdt4936 <sdt4936@gmail.com>
2023-11-03 20:48:19 +0530

    improving readability

--- algorithms/shivsQuickBucket/prims.py
+++ algorithms/shivsQuickBucket/prims.py
@@ -4,20 +4,20 @@ from collections import defaultdict as dd
 import heapq
 # Prims solution
 class Solution:
-    def minimumCost(self, n, cc) -> int:
-        if len(cc) < n - 1: return -1
+    def minimumCost(self, n, connections) -> int:
+        if len(connections) < n - 1: return -1
         visited = set()
-        cc2 = dd(list)
-        for p1, p2, cost in cc:
-            cc2[p1].append((cost, p2))
-            cc2[p2].append((cost, p1))
+        neighbours = dd(list)
+        for p1, p2, cost in connections:
+            neighbours[p1].append((cost, p2))
+            neighbours[p2].append((cost, p1))
         res = 0
-        heap = [(0, cc[0][0])]
+        heap = [(0, connections[0][0])]
         while heap and len(visited) < n:
             cost, node = heapq.heappop(heap)
             if node not in visited:
                 res += cost
                 visited.add(node)
-                for cost, nei in cc2[node]:
+                for cost, nei in neighbours[node]:
                     heapq.heappushpop(heap, (cost, nei))
         return res if len(visited) == n else -1
