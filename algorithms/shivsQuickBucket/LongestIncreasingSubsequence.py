f53b636b4943e4fdac920304162fa942fe5fbe59
sdt4936 <sdt4936@gmail.com>
2023-11-07 21:10:27 +0530

    LIs with explanation

---
+++ algorithms/shivsQuickBucket/LongestIncreasingSubsequence.py
@@ -0,0 +1,24 @@
+# 300. Longest Increasing Subsequence
+   
+''' 
+Logic is: 
+    If the curr element is greater the last element added to the subsequence: 
+    append it to increase the size of the subsequence
+    else 
+    replace a greater element in the subsequence with this smaller element 
+    which will open doors for us to add another element in future 
+    # why else ? -> 
+    we would want to minimize our elements in the subsequence as to 
+    increase our chances of adding a newer element in future
+    However this will not give the correct subsequence because of else statement
+'''
+import bisect
+
+
+def lengthOfLIS(nums) -> int:
+    sub = [nums[0]]
+    for num in nums[1:]:
+        id = bisect.bisect_left(sub, num)
+        if id == len(sub): sub.append(num)
+        else: sub[id] = num # increase our chances of adding a newer element in future
+    return len(sub)
