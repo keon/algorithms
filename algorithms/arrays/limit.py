7ee900a14a4f5aadf487c283636766e25a5e177b
lp07 <114559492+lp07@users.noreply.github.com>
2023-12-20 14:36:24 -0800

    limit.py

--- algorithms/arrays/limit.py
+++ algorithms/arrays/limit.py
@@ -1,11 +1,15 @@
+# Corrected grammatical errors.
+# result -> results, need the -> need to, need value -> need a value, under than 100 -> remove than
+# By use this algorithms -> By using this algorithm, specific -> a specific, array -> an array
+# of given -> of a given, 'unlimit' -> unlimited
 """
-Sometimes you need to limit array result to use. Such as you only need the 
- value over 10 or, you need value under than 100. By use this algorithms, you
- can limit your array to specific value
+Sometimes you need to limit array results to use. Such as you only need to
+ value over 10 or, you need a value under 100. By using this algorithm, you
+ can limit your array to a specific value
 
-If array, Min, Max value was given, it returns array that contains values of 
- given array which was larger than Min, and lower than Max. You need to give
- 'unlimit' to use only Min or Max.
+If an array, Min, Max value was given, it returns an array that contains values of 
+ a given array which was larger than Min, and lower than Max. You need to give
+ 'unlimited' to use only Min or Max.
 
 ex) limit([1,2,3,4,5], None, 3) = [1,2,3]
 
