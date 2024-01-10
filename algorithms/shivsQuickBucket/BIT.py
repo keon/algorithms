fab6eb2fedcdd66cfebc7c845370d879c0c7b117
sdt4936 <sdt4936@gmail.com>
2024-01-11 01:53:36 +0530

    Finally able to get kth smallest element in BIT

--- algorithms/shivsQuickBucket/BIT.py
+++ algorithms/shivsQuickBucket/BIT.py
@@ -16,3 +16,15 @@ class BIT:
             total += self.tree[idx]
             idx -= idx&-idx
         return total
+    
+    # finding kth smallest element
+    def find(self, k):
+        l, r = 1, self.n
+        while l <= r:
+            m = (l + r) >> 1
+            if self.query(m) >= k:
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        return res
