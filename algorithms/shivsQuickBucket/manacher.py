4ecea07f6779081fb5c8db6cbcb654bdfb8491d3
sdt4936 <sdt4936@gmail.com>
2024-01-23 18:36:09 +0530

    adding comments

--- algorithms/shivsQuickBucket/manacher.py
+++ algorithms/shivsQuickBucket/manacher.py
@@ -8,11 +8,11 @@ def manacher(s):
     P = [0] * n
 
     for i in range(1, n - 1):
-        if i < r:
+        if i < r: # update P[i] from it's mirror seen before centre
             P[i] = min(r - i, P[2 * c - i])
         while news[i + P[i] + 1] == news[i - P[i] - 1]:
             P[i] += 1
-        if i + P[i] > r:
+        if i + P[i] > r: # if we have come after r update r for future Is
             c, r = i, i + P[i]
     
     res = resid = 0
@@ -21,4 +21,5 @@ def manacher(s):
             res = val
             resid = id
     
+    # since our res is calculate on news we have to half it for the answer
     return res, s[(resid - res) >> 1: (resid + res) >> 1]
