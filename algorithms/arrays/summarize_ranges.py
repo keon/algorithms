40c944c0457168749f66095e9b7e86f85c3fa709
zlhanq <127005873+zlhanq@users.noreply.github.com>
2024-02-06 07:01:59 +0800

    Update summarize_ranges.py (#912)

--- algorithms/arrays/summarize_ranges.py
+++ algorithms/arrays/summarize_ranges.py
@@ -6,22 +6,20 @@ For example, given [0, 1, 2, 4, 5, 7], return [(0, 2), (4, 5), (7, 7)].
 """
 
 
-def summarize_ranges(array):
-    """
-    :type array: List[int]
-    :rtype: List[]
-    """
+from typing import List
+
+def summarize_ranges(array: List[int]) -> List[str]:
     res = []
     if len(array) == 1:
         return [str(array[0])]
-    i = 0
-    while i < len(array):
-        num = array[i]
-        while i + 1 < len(array) and array[i + 1] - array[i] == 1:
-            i += 1
-        if array[i] != num:
-            res.append((num, array[i]))
+    it = iter(array)
+    start = end = next(it)
+    for num in it:
+        if num - end == 1:
+            end = num
         else:
-            res.append((num, num))
-        i += 1
-    return res
+            res.append((start, end) if start != end else (start,))
+            start = end = num
+    res.append((start, end) if start != end else (start,))
+    return [f"{r[0]}-{r[1]}" if len(r) > 1 else str(r[0]) for r in res]
+
