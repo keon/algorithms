123ef62cc9c2f5f997a89b841e5b221e9e941e0b
ca20110820 <ca20110820@gmail.com>
2024-04-14 12:07:45 +0800

    fix: fixed test_summarize_ranges method in test_array

--- tests/test_array.py
+++ tests/test_array.py
@@ -354,11 +354,11 @@ class TestSummaryRanges(unittest.TestCase):
     def test_summarize_ranges(self):
 
         self.assertListEqual(summarize_ranges([0, 1, 2, 4, 5, 7]),
-                             [(0, 2), (4, 5), (7, 7)])
+                             ['0-2', '4-5', '7'])
         self.assertListEqual(summarize_ranges([-5, -4, -3, 1, 2, 4, 5, 6]),
-                             [(-5, -3), (1, 2), (4, 6)])
+                             ['-5--3', '1-2', '4-6'])
         self.assertListEqual(summarize_ranges([-2, -1, 0, 1, 2]),
-                             [(-2, 2)])
+                             ['-2-2'])
 
 
 class TestThreeSum(unittest.TestCase):
