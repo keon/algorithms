63f03156d1de86156357f0fc19289e07066b3e5e
sdt4936 <sdt4936@gmail.com>
2024-05-04 02:48:40 +0530

    improvement on dijkstra's

--- algorithms/shivsQuickBucket/quickDijkstra's.py
+++ algorithms/shivsQuickBucket/quickDijkstra's.py
@@ -7,20 +7,18 @@ class Dijkstra():
         return
     
     @staticmethod
-    def get_shortest_path(distances, src):
-        n = len(distances)
-        min_distance = [inf] * n
+    def get_shortest_path(adj, src):
+        n = len(adj)
+        min_d = [inf] * n
         heap = [(0, src)]
-        min_distance[src] = 0
-        visited = set()
+        min_d[src] = 0
 
         while heap:
             cost, node = heappop(heap)
-            if node in visited: continue
-            visited.add(node)
-            for new_node, new_cost in distances[node]:
+            if cost > min_d[node]: continue
+            for new_node, new_cost in adj[node]:
                 total_cost = cost + new_cost
-                if total_cost < min_distance[new_node]:
-                    min_distance[new_node] = total_cost
+                if total_cost < min_d[new_node]:
+                    min_d[new_node] = total_cost
                     heappush(heap, (total_cost, new_node))
-        return min_distance
+        return min_d
