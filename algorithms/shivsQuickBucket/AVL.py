e285fb39331ad0b4e39dab202bf53bc39b25ad49
sdt4936 <sdt4936@gmail.com>
2024-05-04 00:58:12 +0530

    making code readable

--- algorithms/shivsQuickBucket/AVL.py
+++ algorithms/shivsQuickBucket/AVL.py
@@ -19,29 +19,7 @@ class TreeNode:
         root.height = 1 + max(self.getHeight(root.left),
                               self.getHeight(root.right))
 
-        # Step 3 - Get the balance factor
-        balance = self.getBalance(root)
-
-        # Step 4 - If the node is unbalanced, then try out the 4 cases
-        # Case 1 - Left Left
-        if balance > 1 and key < root.left.val:
-            return self.rightRotate(root)
-
-        # Case 2 - Right Right
-        if balance < -1 and key > root.right.val:
-            return self.leftRotate(root)
-
-        # Case 3 - Left Right
-        if balance > 1 and key > root.left.val:
-            root.left = self.leftRotate(root.left)
-            return self.rightRotate(root)
-
-        # Case 4 - Right Left
-        if balance < -1 and key < root.right.val:
-            root.right = self.rightRotate(root.right)
-            return self.leftRotate(root)
-
-        return root
+        return self.balance(root, key)
 
     # Recursive function to delete a node with
     # given key from subtree with given root.
@@ -80,29 +58,7 @@ class TreeNode:
         root.height = 1 + max(self.getHeight(root.left),
                               self.getHeight(root.right))
 
-        # Step 3 - Get the balance factor
-        balance = self.getBalance(root)
-
-        # Step 4 - If the node is unbalanced, then try out the 4 cases
-        # Case 1 - Left Left
-        if balance > 1 and self.getBalance(root.left) >= 0:
-            return self.rightRotate(root)
-
-        # Case 2 - Right Right
-        if balance < -1 and self.getBalance(root.right) <= 0:
-            return self.leftRotate(root)
-
-        # Case 3 - Left Right
-        if balance > 1 and self.getBalance(root.left) < 0:
-            root.left = self.leftRotate(root.left)
-            return self.rightRotate(root)
-
-        # Case 4 - Right Left
-        if balance < -1 and self.getBalance(root.right) > 0:
-            root.right = self.rightRotate(root.right)
-            return self.leftRotate(root)
-
-        return root
+        return self.balance(root, key)
 
     def leftRotate(self, z):
 
@@ -155,6 +111,31 @@ class TreeNode:
             return root
  
         return self.getMinValueNode(root.left)
+    
+    def rebalance(self, root, key):
+        # Step 3 - Get the balance factor
+        balance = self.getBalance(root)
+
+        # Step 4 - If the node is unbalanced, then try out the 4 cases
+        # Case 1 - Left Left
+        if balance > 1 and key < root.left.val:
+            return self.rightRotate(root)
+
+        # Case 2 - Right Right
+        if balance < -1 and key > root.right.val:
+            return self.leftRotate(root)
+
+        # Case 3 - Left Right
+        if balance > 1 and key > root.left.val:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+
+        # Case 4 - Right Left
+        if balance < -1 and key < root.right.val:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
 
     def preOrder(self, root):
         if not root:
