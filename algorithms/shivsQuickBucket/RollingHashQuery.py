964e1977c64a715c385f529da823428cbbfb1ec2
sdt4936 <sdt4936@gmail.com>
2024-05-29 22:04:51 +0530

    adding more comments

--- algorithms/shivsQuickBucket/RollingHashQuery.py
+++ algorithms/shivsQuickBucket/RollingHashQuery.py
@@ -1,16 +1,16 @@
 class prefixHash:
     def __init__(self ,s):
         self.mod = mod = 344555666677777
-        self.base = base = 10 ** 7 + 7
-        #fermat's little theorem since base and mod are co-prime
-        self.invbase = invbase = pow(base, mod - 2, mod)
-        self.prefix = prefix = [0]
+        self.basePower = basePower = 10 ** 7 + 7
+        #fermat's little theorem since basePower and mod are co-prime
+        self.invbase = invbase = pow(basePower, mod - 2, mod)
+        self.prefix = prefix = [0] # r - l in query so for 1st prefix -> l needs to be 0 right?
         self.invprefix = invprefix = [1]
-        power, invpower, hash = 1, 1, 0
+        hash, power, invpower = 0, 1, 1
 
         for x in s:
             hash = (hash + x * power) % mod
-            power = (power * base) % mod
+            power = (power * basePower) % mod
             invpower = (invpower * invbase) % mod
             prefix.append(hash)
             invprefix.append(invpower)
