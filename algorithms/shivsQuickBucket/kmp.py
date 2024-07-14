4d48887e55980f5976524920e0c2549c0b9c1a1e
sdt4936 <sdt4936@gmail.com>
2024-07-14 22:59:45 +0530

    adding more docs

--- algorithms/shivsQuickBucket/kmp.py
+++ algorithms/shivsQuickBucket/kmp.py
@@ -2,7 +2,7 @@
 # as pattern can have repeated values -> only works if this is true
 # that is why we create pi table to store the jump values
 def kmp(text, pattern):
-    m, n = len(text), len(pattern)
+    n, m = len(text), len(pattern)
     pi = [0] * n
 
     j = 0
@@ -10,7 +10,7 @@ def kmp(text, pattern):
     # Examples - P = ABCDABD π = (0, 0, 0, 0, 1, 2, 0)
     # pi table means -> agar yahan khtm hui matching to starting se start karo if 0 is there otherwise start
     # start with the position givne in pi table kyunki wahan tk ka matching already hogya tha
-    for i in range(1, n):
+    for i in range(1, m):
         while j and pattern[i] != pattern[j]:
             j = pi[j - 1]
         if pattern[i] == pattern[j]:
@@ -20,13 +20,13 @@ def kmp(text, pattern):
     # finding pattern in text
     j = 0
     res = []
-    for i in range(m):
+    for i in range(n):
         while j and text[i] != pattern[j]:
             j = pi[j - 1]
         if text[i] == pattern[j]:
             j += 1
-            if j == n:
-                res.append(i - n + 1)
+            if j == m:
+                res.append(i - m + 1)
                 j = pi[j - 1]
                 
     return res
